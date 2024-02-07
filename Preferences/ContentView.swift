5a48827c26aac282147fcbc562ac7b153374bcac
Chris Pineda <zhris@zhr.is>
2024-02-07 17:43:23 -0600

    Initial Commit

---
+++ Preferences/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Preferences
+//
+//  Created by Chris on 2/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Preferences/PreferencesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreferencesApp.swift
+//  Preferences
+//
+//  Created by Chris on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PreferencesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
