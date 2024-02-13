9183b2fb2d1057bcc1a48ec0372f8e14e8cbe659
Chris Pineda <zhris@zhr.is>
2024-02-12 19:55:39 -0600

    Modified ShortcutsView.

--- Preferences/Views/ShortcutsView.swift
+++ Preferences/Views/ShortcutsView.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct ShortcutsView: View {
+    // Variables
+    @State private var cloudSyncEnabled = true
+    @State private var privateSharingEnabled = false
+    
     var body: some View {
-        Text("Hello, World!")
+        CustomList(title: "Shortcuts") {
+            Section {
+                Toggle("iCloud Sync", isOn: $cloudSyncEnabled)
+            }
+            
+            Section(content: {
+                Toggle("Private Sharing", isOn: $privateSharingEnabled)
+            }, footer: {
+                Text("Allow receiving shortcuts directly from people in your contacts. Apple cannot verify the authenticity of shortcuts shared privately. [About Shortcuts Sharing & Privacy...](#)")
+            })
+            
+            Section {
+                NavigationLink("Advanced", destination: {})
+            }
+            
+            Section {
+                NavigationLink("Legal Notices", destination: {})
+            }
+        }
     }
 }
 
