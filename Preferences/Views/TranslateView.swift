966ac3dbc8e889fd51e6c274aed95d7ebd006eaf
Chris Pineda <zhris@zhr.is>
2024-02-12 20:02:38 -0600

    Modified TranslateView.

--- Preferences/Views/TranslateView.swift
+++ Preferences/Views/TranslateView.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct TranslateView: View {
+    // Variables
+    @State private var onDeviceModeEnabled = false
+    
     var body: some View {
-        Text("Hello, World!")
+        CustomList(title: "Translate") {
+            Section(content: {
+                NavigationLink("Downloaded Languages", destination: {})
+            }, footer: {
+                Text("Download languages to translate when offline or when on-device mode is turned on.")
+            })
+            
+            Section(content: {
+                Toggle("On-Device Mode", isOn: $onDeviceModeEnabled)
+            }, footer: {
+                Text("Always translate offline using downloaded languages. Offline translations may not be as accurate as online translations. Siri and Safari will always process translations online.")
+            })
+            
+            Section(content: {}, footer: {
+                Text("[About Translation & Privacy...](#)")
+            })
+        }
     }
 }
 
