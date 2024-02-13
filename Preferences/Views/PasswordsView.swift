f977e72229e50002d8d35a916cd499f3cd6ad972
Chris Pineda <zhris@zhr.is>
2024-02-12 19:48:55 -0600

    Modified PasswordsView to finalize appearance.

--- Preferences/Views/PasswordsView.swift
+++ Preferences/Views/PasswordsView.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct PasswordsView: View {
+    // Variables
+    @State private var searchText = String()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        CustomList(title: "Passwords") {
+            Section {
+                SettingsLink(color: .green, icon: "shield.lefthalf.filled.badge.checkmark", larger: false, id: "Security Recommendations", subtitle: "No issues found", status: "0", content: {})
+                SettingsLink(color: .gray, icon: "switch.2", larger: false, id: "Password Options", content: {})
+            }
+        }
+        .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing, content: {
+                Button("\(Image(systemName: "plus"))", action: {})
+            })
+            ToolbarItem(placement: .topBarTrailing, content: {
+                EditButton()
+                    .disabled(true)
+            })
+        }
+        
     }
 }
 
