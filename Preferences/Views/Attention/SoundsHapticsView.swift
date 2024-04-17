0dff22595760b1170fe8e6086bfbade659428b77
Chris Pineda <zhris@zhr.is>
2024-04-16 22:25:00 -0500

    Modified SoundsHapticsView.

--- Preferences/Views/Attention/SoundsHapticsView.swift
+++ Preferences/Views/Attention/SoundsHapticsView.swift
@@ -11,6 +11,8 @@ struct SoundsHapticsView: View {
     // Variables
     @State private var silentModeEnabled = true
     @State private var showInStatusBarEnabled = true
+    @State private var volume = 0.5
+    @State private var changeWithButtonsEnabled = true
     
     var body: some View {
         CustomList(title: DeviceInfo().isPhone ? "Sounds & Haptics" : "Sounds") {
@@ -34,6 +36,40 @@ struct SoundsHapticsView: View {
             
             Section {
                 Toggle("Show in Status Bar", isOn: $showInStatusBarEnabled)
+            }
+            
+            Section(content: {
+                Group {
+                    Slider(value: $volume,
+                           in: 0...100,
+                           minimumValueLabel: Image(systemName: "speaker.fill"),
+                           maximumValueLabel: Image(systemName: "speaker.wave.3.fill"),
+                           label: {
+                               Text("Volume")
+                           }
+                    )
+                }
+                .foregroundStyle(.secondary)
+                
+                Toggle("Change with Buttons", isOn: $changeWithButtonsEnabled)
+                if DeviceInfo().isPhone {
+                    CustomNavigationLink(title: "Haptics", status: "Always Play", destination: EmptyView())
+                }
+            }, header: {
+                Text("Ringtone and Alerts")
+            }, footer: {
+                Text("The volume buttons \(changeWithButtonsEnabled ? "can be used to adjust" : "will not affect") the volume of the ringtone and alerts.")
+            })
+            
+            Section {
+                CustomNavigationLink(title: "Ringtone", status: "Reflection", destination: EmptyView())
+                CustomNavigationLink(title: "Text Tone", status: "Note", destination: EmptyView())
+                CustomNavigationLink(title: "New Voicemail", status: "Droplet", destination: EmptyView())
+                CustomNavigationLink(title: "New Mail", status: "None", destination: EmptyView())
+                CustomNavigationLink(title: "Sent Mail", status: "Swoosh", destination: EmptyView())
+                CustomNavigationLink(title: "Calendar Alerts", status: "Chord", destination: EmptyView())
+                CustomNavigationLink(title: "Reminder Alerts", status: "Chord", destination: EmptyView())
+                CustomNavigationLink(title: "Default Alerts", status: "Rebound", destination: EmptyView())
             }
         }
     }
