944400de428fcd55af7ebc853870344186a32841
Chris <zhris@zhr.is>
2024-06-26 15:32:37 -0400

    Added NameView. Modified AboutView.

---
+++ Preferences/Views/Main/General/About/NameView.swift
@@ -0,0 +1,52 @@
+//
+//  NameView.swift
+//  Preferences
+//
+//  Settings > General > About > Name
+//
+
+import SwiftUI
+
+struct NameView: View {
+    // Variables
+    @AppStorage("DeviceName") private var deviceName = Device().model
+    @Environment(\.dismiss) private var dismiss
+    @FocusState private var focusedName: Bool
+    
+    var body: some View {
+        CustomList(title: "Name") {
+            HStack {
+                TextField("Name", text: $deviceName, prompt: Text(Device().model))
+                    .focused($focusedName)
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            self.focusedName = true
+                        }
+                    }
+                    .onSubmit {
+                        if deviceName.isEmpty {
+                            deviceName = Device().model
+                        }
+                        dismiss()
+                    }
+                if deviceName.count > 0 {
+                    Button {
+                        deviceName = ""
+                    } label: {
+                        Image(systemName: "xmark.circle.fill")
+                            .foregroundStyle(Color.background, Color(UIColor.separator))
+                    }
+                    .foregroundStyle(.gray)
+                    .buttonStyle(.plain)
+                }
+            }
+        }
+
+    }
+}
+
+#Preview {
+    NavigationStack {
+        NameView()
+    }
+}
--- Preferences/Views/Main/General/AboutView.swift
+++ Preferences/Views/Main/General/AboutView.swift
@@ -13,6 +13,7 @@ struct AboutView: View {
     @State private var serialNumber = String()
     @State private var availableStorage: String = getAvailableStorage() ?? "N/A"
     @State private var totalStorage: String = getTotalStorage() ?? "N/A"
+    @AppStorage("DeviceName") private var deviceName = Device().model
     
     var body: some View {
         CustomList(title: "About") {
@@ -20,7 +21,7 @@ struct AboutView: View {
                 if Configuration().isSimulator {
                     LabeledContent("Name", value: Device().model)
                 } else {
-                    CustomNavigationLink(title: "Name", status: Device().model, destination: EmptyView())
+                    CustomNavigationLink(title: "Name", status: deviceName, destination: NameView())
                 }
                 
                 CustomNavigationLink(title: "\(UIDevice().systemName) Version", status: UIDevice().systemVersion, destination: VersionView())
@@ -35,15 +36,6 @@ struct AboutView: View {
                         serialNumber = randomSerialNumber()
                     }
             }
-            
-//            Section {
-//                CustomNavigationLink(title: "Limited Warranty", status: "Expires: 1/1/25", destination: EmptyView())
-//                Button {} label: {
-//                    Text("Add AppleCare+ Coverage")
-//                }
-//            } footer: {
-//                Text("There are 60 days left to add coverage for accidental damage.")
-//            }
             
             Section {
                 LabeledContent("Songs", value: "0")
