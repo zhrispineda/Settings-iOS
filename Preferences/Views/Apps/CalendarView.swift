a3d3fcd9aa4c132a9b272859ecfb88ebd8f9eedd
Chris <zhris@zhr.is>
2024-07-03 11:11:37 -0400

    Modified CalendarView and HealthView.

--- Preferences/Views/Apps/CalendarView.swift
+++ Preferences/Views/Apps/CalendarView.swift
@@ -17,8 +17,8 @@ struct CalendarView: View {
     var body: some View {
         CustomList(title: "Calendar") {
             Section {
-                SectionHelp(title: "Calendar", icon: "appleCalendar", description: "Add and manage your accounts to create and edit events in the Calendar app. [Learn more...](#)")
-                NavigationLink("Calendar Accounts", destination: {})
+                SectionHelp(title: "Calendar", icon: "appleCalendar", description: "Add and manage your accounts to create and edit events in the Calendar app. [Learn more...](https://support.apple.com/guide/\(Device().isPhone ? "iphone/set-up-mail-contacts-and-calendar-accounts-ipha0d932e96/ios"  : "ipad/set-up-mail-contacts-and-calendar-accounts-ipadee835d39/ipados"))")
+                CustomNavigationLink(title: "Calendar Accounts", status: "1", destination: EmptyView())
             }
             
             PermissionsView(appName: "Calendar", cellular: false, location: false, notifications: false, cellularEnabled: .constant(false))
@@ -31,11 +31,13 @@ struct CalendarView: View {
                 Toggle("Week Numbers", isOn: $weekNumbersEnabled)
                 Toggle("Week View Starts On Today", isOn: $weekViewStartsTodayEnabled)
                 Toggle("Show Invitee Declines", isOn: $showInviteeDeclinesEnabled)
-                CustomNavigationLink(title: "Sync", status: "All Events", destination: EmptyView())
+                if !UIDevice.isSimulator {
+                    CustomNavigationLink(title: "Sync", status: "All Events", destination: EmptyView())
+                }
                 NavigationLink("Default Alert Times") {}
                 CustomNavigationLink(title: "Duration for New Events", status: "1 hour", destination: EmptyView())
                 NavigationLink("Start Week On") {}
-                CustomNavigationLink(title: "Default Calendar", status: "Personal", destination: EmptyView())
+                //CustomNavigationLink(title: "Default Calendar", status: "Personal", destination: EmptyView())
                 Toggle("Location Suggestions", isOn: $locationSuggestionsEnabled)
             }
         }
--- Preferences/Views/Apps/HealthView.swift
+++ Preferences/Views/Apps/HealthView.swift
@@ -10,17 +10,13 @@ import SwiftUI
 struct HealthView: View {
     var body: some View {
         CustomList(title: "Health") {
-            Section {
-                SettingsLink(icon: "appleSiri", id: "Siri & Search") {
-                    SiriDetailView(appName: "Health")
-                }
-            } header: {
-                Text("Allow Health to Access")
-            }
+            PermissionsView(appName: "Health", cellular: false, location: false, cellularEnabled: .constant(false))
             
             Section {
                 NavigationLink("Health Details", destination: HealthDetailsView())
-                NavigationLink("Medical ID", destination: MedicalView())
+                if Device().isPhone {
+                    NavigationLink("Medical ID", destination: MedicalView())
+                }
             } header: {
                 Text("Medical Details")
             }
@@ -29,6 +25,14 @@ struct HealthView: View {
                 NavigationLink("Data Access & Devices", destination: DataAccessDevicesView())
             } header: {
                 Text("Data")
+            }
+            
+            if Device().isPhone {
+                Section {
+                    Link("Open Health Checklist", destination: URL(string: "x-apple-health://")!)
+                } footer: {
+                    Text("Health Checklist can help set up your iPhone and Apple Watch to keep an eye on things for you.")
+                }
             }
         }
     }
