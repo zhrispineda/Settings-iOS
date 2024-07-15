8d988f8dc53e00fda874360f51d96ab77fc9e2bb
Chris <zhris@zhr.is>
2024-07-15 11:22:23 -0400

    Added BatteryUI localization. Modified BatteryView.

--- Preferences/Views/Radios/BatteryView.swift
+++ Preferences/Views/Radios/BatteryView.swift
@@ -11,25 +11,26 @@ struct BatteryView: View {
     // Variables
     @State private var batteryPercentageEnabled = false
     @State private var lowPowerModeEnabled = false
+    let table = "BatteryUI"
     
     var body: some View {
-        CustomList(title: "BATTERY_TITLE") {
+        CustomList(title: "BATTERY_TITLE".localize(table: table)) {
             Section {
-                Toggle("BATTERY_PERCENTAGE", isOn: $batteryPercentageEnabled)
-                Toggle("BATTERY_SAVER_MODE", isOn: $lowPowerModeEnabled)
+                Toggle("BATTERY_PERCENTAGE".localize(table: table), isOn: $batteryPercentageEnabled)
+                Toggle("BATTERY_SAVER_MODE".localize(table: table), isOn: $lowPowerModeEnabled)
             } footer: {
-                Text(UIDevice.iPhone ? "FOOTNOTE_BATTERYSAVERMODE_IPHONE" : "FOOTNOTE_BATTERYSAVERMODE_IPAD")
+                Text(UIDevice.iPhone ? "FOOTNOTE_BATTERYSAVERMODE_IPHONE".localize(table: table) : "FOOTNOTE_BATTERYSAVERMODE_IPAD".localize(table: table))
             }
             
             Section {
                 if UIDevice.DeviceSupportsBatteryInformation {
-                    CustomNavigationLink(title: "BATTERY_HEALTH_TITLE", status: "NORMAL_STATE", destination: BatteryHealthView())
+                    CustomNavigationLink(title: "BATTERY_HEALTH_TITLE".localize(table: table), status: "NORMAL_STATE".localize(table: table), destination: BatteryHealthView())
                     if UIDevice.iPhone {
-                        NavigationLink("CHARGING_TITLE_CHARGING", destination: ChargingOptimizationView())
+                        NavigationLink("CHARGING_TITLE_CHARGING".localize(table: table), destination: ChargingOptimizationView())
                     }
                 } else {
                     if UIDevice.iPhone {
-                        NavigationLink("BATTERY_HEALTH", destination: BatteryHealthChargingView())
+                        NavigationLink("BATTERY_HEALTH".localize(table: table), destination: BatteryHealthChargingView())
                     }
                 }
             }
