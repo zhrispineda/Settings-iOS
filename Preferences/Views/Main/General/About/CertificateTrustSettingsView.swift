da92f0b5b3f688091c803335e7e1cda36dd2b7a3
Chris <zhris@zhr.is>
2024-07-27 19:56:07 -0500

    Replace iCloud assets. Modified CertificateTrustSettingsView.

--- Preferences/Views/Main/General/About/CertificateTrustSettingsView.swift
+++ Preferences/Views/Main/General/About/CertificateTrustSettingsView.swift
@@ -11,7 +11,7 @@ struct CertificateTrustSettingsView: View {
     var body: some View {
         CustomList(title: "Certificate Trust Settings") {
             Section {
-                LabeledContent("Trust Store Version", value: "2024040500")
+                LabeledContent("Trust Store Version", value: "2024051501")
                 LabeledContent("Trust Asset Version", value: "1003")
             } footer: {
                 Text("[Learn more about trusted certificates](https://support.apple.com/kb/HT5012)")
