bae95dae625e0a66ce895f20af95ded69246149d
Chris <zhris@zhr.is>
2024-09-11 20:14:12 -0400

    Modified FaceAttentionView and MotionView to be updated.

--- Preferences/Views/Main/Accessibility/FaceAttentionView.swift
+++ Preferences/Views/Main/Accessibility/FaceAttentionView.swift
@@ -11,13 +11,16 @@ struct FaceAttentionView: View {
     // Variables
     @State private var requireAttentionEnabled = true
     @State private var attentionAwareFeaturesEnabled = true
+    @State private var hapticAuthentication = false
     
     var body: some View {
         CustomList(title: "Face ID & Attention") {
-            Section {
-                Toggle("Require Attention for Face ID", isOn: $requireAttentionEnabled)
-            } footer: {
-                Text("TrueDepth camera will provide an additional level of security by verifying that you are looking at \(UIDevice.current.model) before unlocking. Some sunglasses may block attention detection.")
+            if !UIDevice.isSimulator {
+                Section {
+                    Toggle("Require Attention for Face ID", isOn: $requireAttentionEnabled)
+                } footer: {
+                    Text("TrueDepth camera will provide an additional level of security by verifying that you are looking at \(UIDevice.current.model) before unlocking. Some sunglasses may block attention detection.")
+                }
             }
             
             Section {
@@ -27,6 +30,16 @@ struct FaceAttentionView: View {
                     Text("iPhone will check for attention before dimming the display, expanding a notification when locked, or lowering the volume of some alerts.")
                 } else if UIDevice.iPad {
                     Text("iPad will check for attention before dimming the display or lowering the volume of some alerts.")
+                }
+            }
+            
+            if !UIDevice.isSimulator && UIDevice.iPhone {
+                Section {
+                    Toggle("Haptic on Successful Authentication", isOn: $hapticAuthentication)
+                } header: {
+                    Text("Haptics")
+                } footer: {
+                    Text("Play a haptic when Face ID successfully unlocks iPhone, authorizes Apple Pay, or verifies iTunes and App Store purchases.")
                 }
             }
         }
--- Preferences/Views/Main/Accessibility/MotionView.swift
+++ Preferences/Views/Main/Accessibility/MotionView.swift
@@ -11,10 +11,11 @@ struct MotionView: View {
     // Variables
     @State private var reduceMotionEnabled = false
     @State private var preferCrossFadeTransitionsEnabled = false
-    @State private var autoPlayMessageEffectsEnabled = true
-    @State private var autoPlayAnimatedImagesEnabled = true
     @State private var dimFlashingLightsEnabled = false
+    @State private var autoPlayAnimatedImagesEnabled = true
     @State private var autoPlayVideoPreviewsEnabled = true
+    @State private var autoPlayMessageEffectsEnabled = true
+    @State private var preferNonBlinkingCursor = false
     @State private var limitFrameRateEnabled = false
     
     var body: some View {
@@ -34,25 +35,23 @@ struct MotionView: View {
             }
             
             Section {
-                Toggle("Auto-Play Message Effects", isOn: $autoPlayMessageEffectsEnabled)
+                Toggle("Dim Flashing Lights", isOn: $dimFlashingLightsEnabled)
             } footer: {
-                Text("Allows fullscreen effects in the Messages app to auto-play.")
+                Text("Video content that depicts repeated flashing or strobing lights will be automatically dimmed.")
             }
             
             Section {
                 Toggle("Auto-Play Animated Images", isOn: $autoPlayAnimatedImagesEnabled)
-            } footer: {
-                Text("Controls whether images animate on the Web and in apps.")
-            }
-            
-            Section {
-                Toggle("Dim Flashing Lights", isOn: $dimFlashingLightsEnabled)
-            } footer: {
-                Text("Video content that depicts repeated flashing or strobing lights will be automatically dimmed. The video timeline will display when flashing lights occur in the content for supported media.")
-            }
-            
-            Section {
                 Toggle("Auto-Play Video Previews", isOn: $autoPlayVideoPreviewsEnabled)
+                Toggle("Auto-Play Message Effects", isOn: $autoPlayMessageEffectsEnabled)
+            } footer: {
+                Text("Control if videos, animated imags, and full screen effects in messages play automatically.")
+            }
+            
+            Section {
+                Toggle("Prefer Non-Blinking Cursor", isOn: $preferNonBlinkingCursor)
+            } footer: {
+                Text("Prefer the cursor indicator in text does not blink.")
             }
             
             if UIDevice.ProDevice {
