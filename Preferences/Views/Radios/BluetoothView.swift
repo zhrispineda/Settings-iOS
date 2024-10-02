70b9d557199c5f4b6245da883293db299241386b
Chris <zhris@zhr.is>
2024-10-02 11:37:46 -0400

    Added Devices xcstrings for use in BluetoothView. Organized Accessibility and BluetoothSettings xcstrings into groups.

--- Preferences/Views/Radios/BluetoothView.swift
+++ Preferences/Views/Radios/BluetoothView.swift
@@ -13,11 +13,12 @@ struct BluetoothView: View {
     @AppStorage("DeviceName") private var deviceName = UIDevice.current.model
     @State private var opacity: Double = 0
     @State private var frameY: Double = 0
+    let table = "Devices"
     
     var body: some View {
         CustomList {
             Section {
-                SectionHelp(title: "BLUETOOTH".localize(table: "BluetoothSettings"), color: Color.blue, icon: "bluetooth", description: "\("BLUETOOTHPLACARDINFO".localize(table: "BluetoothSettings")) [\("LEARN_MORE".localize(table: "BluetoothSettings"))](https://support.apple.com/guide/\(UIDevice.iPhone ?  "iphone/bluetooth-accessories-iph3c50f191/ios" : "ipad/bluetooth-accessories-ipad997da4cf/ipados"))")
+                SectionHelp(title: "BLUETOOTH".localize(table: table), color: Color.blue, icon: "bluetooth", description: "\("BLUETOOTHPLACARDINFO".localize(table: table)) [\("LEARN_MORE".localize(table: table))](https://support.apple.com/guide/\(UIDevice.iPhone ?  "iphone/bluetooth-accessories-iph3c50f191/ios" : "ipad/bluetooth-accessories-ipad997da4cf/ipados"))")
                     .overlay { // For calculating opacity of the principal toolbar item
                         GeometryReader { geo in
                             Color.clear
@@ -27,12 +28,12 @@ struct BluetoothView: View {
                                 }
                         }
                     }
-                Toggle("BLUETOOTH".localize(table: "BluetoothSettings"), isOn: $bluetoothEnabled.animation())
+                Toggle("BLUETOOTH".localize(table: table), isOn: $bluetoothEnabled.animation())
             } footer: {
                 if bluetoothEnabled {
-                    Text("DISCOVERABLE".localize(table: "BluetoothSettings", deviceName))
+                    Text("DISCOVERABLE".localize(table: table, deviceName))
                 } else {
-                    Text("POWER_OFF_WARNING", tableName: "BluetoothSettings")
+                    Text("POWER_OFF_WARNING", tableName: table)
                 }
             }
             
@@ -41,20 +42,21 @@ struct BluetoothView: View {
                     EmptyView()
                 } header: {
                     HStack {
-                        Text("DEVICES", tableName: "BluetoothSettings")
+                        Text("DEVICES", tableName: table)
                         ProgressView()
                             .padding(.horizontal, 1)
                     }
                 } footer: {
                     if UIDevice.iPhone {
-                        Text(.init("APPLE_WATCH_FOOTER_TEXT".localize(table: "BluetoothSettings", "[\("APPLE_WATCH_APP_LINK".localize(table: "BluetoothSettings"))](itms-watchs://)")))
+                        Text(.init("APPLE_WATCH_FOOTER_TEXT".localize(table: table, "[\("APPLE_WATCH_APP_LINK".localize(table: table))](itms-watchs://)")))
                     }
                 }
             }
         }
         .toolbar {
             ToolbarItem(placement: .principal) {
-                Text("**\("BLUETOOTH".localize(table: "BluetoothSettings"))**")
+                Text("\("BLUETOOTH".localize(table: "table"))")
+                    .fontWeight(.semibold)
                     .font(.subheadline)
                     .opacity(frameY < 50.0 ? opacity : 0) // Only fade when passing the help section title at the top
             }
