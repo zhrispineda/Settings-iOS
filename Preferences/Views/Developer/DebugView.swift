08418bdcbd75f0f99c0c9a72408bdd9afba33752
Chris <zhris@zhr.is>
2025-03-17 23:39:53 -0400

    Update DebugView

--- Preferences/Views/Developer/DebugView.swift
+++ Preferences/Views/Developer/DebugView.swift
@@ -6,37 +6,58 @@
 import SwiftUI
 
 struct DebugView: View {
+    // Variables
+    @Environment(\.dismiss) private var dismiss
+    #if DEBUG
+    let debugBuild = true
+    #else
+    let debugBuild = false
+    #endif
+    
     var body: some View {
         List {
             Section {
-                Button("") {}
+                Button("Reset app navigation state.") {}
             }
             
             Section {
-                NavigationLink("") {}
-                NavigationLink("") {}
+                NavigationLink("Search") {}
+                NavigationLink("Debug Settings") {}
             }
             
-            Section("") {
-                LabeledContent("", value: "")
-                LabeledContent("", value: "")
+            Section("Info") {
+                LabeledContent("Version", value: "1308.2.502 (1)")
+                LabeledContent("Compiled With Debug", value: debugBuild ? "Yes" : "No")
             }
             
-            Section("") {
-                LabeledContent("", value: "")
-                LabeledContent("", value: "")
-                LabeledContent("", value: "")
-                LabeledContent("", value: "")
-                LabeledContent("", value: "")
-                LabeledContent("", value: "")
-                LabeledContent("", value: "")
-                LabeledContent("", value: "")
-                LabeledContent("", value: "")
+            Section("Bundles") {
+                LabeledContent("GeneralSettingsUI", value: "1200.1.5 (1.0)")
+                LabeledContent("LegalAndRegulatorySettings", value: "1044.1.1 (1044.1.1)")
+                LabeledContent("Preferences Framework", value: "5264.1.5 (1)")
+                LabeledContent("Preferences UI Framework", value: "1308.2.502 (1)")
+                LabeledContent("PrivacySettingsUI", value: "1203.1.5 (1.0)")
+                LabeledContent("Settings", value: "207.1.3.100 (207.1.3.100)")
+                LabeledContent("SettingsFoundation", value: "1080.1.2 (1.0)")
+                LabeledContent("SettingsUIKitPrivate", value: "1066.1.3 (1.0)")
+                LabeledContent("SoundsAndHapticsSettings", value: "1098.1.3 (1.0)")
             }
         }
         .toolbar {
+            ToolbarItem(placement: .principal) {
+                HStack {
+                    Image(.appleSettings)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 32)
+                        .clipShape(RoundedRectangle(cornerRadius: 6))
+                    Text("Debug")
+                }
+            }
+            
             ToolbarItem(placement: .topBarTrailing) {
-                Button("") {}
+                Button("Dismiss") {
+                    dismiss()
+                }
             }
         }
     }
