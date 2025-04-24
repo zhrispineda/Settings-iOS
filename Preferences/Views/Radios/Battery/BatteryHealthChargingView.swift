4bf88a7a557487dc188a679014305301b8c8c8c6
Chris <zhrispineda@icloud.com>
2025-04-24 13:30:02 -0500

    Update BatteryHealthChargingView

--- Preferences/Views/Radios/Battery/BatteryHealthChargingView.swift
+++ Preferences/Views/Radios/Battery/BatteryHealthChargingView.swift
@@ -8,22 +8,18 @@
 import SwiftUI
 
 struct BatteryHealthChargingView: View {
-    // Variables
-    @State private var maximumCapacity = 100
-    
-    @AppStorage("OptimizedBatteryChargingToggle") private var optimizedBatteryChargingEnabled = true
-    @State private var showingOptimizeWarning = false
-    @AppStorage("OptimizedChargingPaused") private var temporaryOptimizePauseEnabled = false
-    
     @AppStorage("CleanEnergyChargingToggle") private var cleanEnergyChargingEnabled = true
-    @State private var showingCleanEnergyWarning = false
+    @AppStorage("OptimizedBatteryChargingToggle") private var optimizedBatteryChargingEnabled = true
     @AppStorage("CleanChargingPaused") private var temporaryCleanPauseEnabled = false
-    
+    @AppStorage("OptimizedChargingPaused") private var temporaryOptimizePauseEnabled = false
+    @State private var maximumCapacity = 100
+    @State private var showingCleanEnergyWarning = false
+    @State private var showingOptimizeWarning = false
     let table = "BatteryUI"
     
     var body: some View {
         CustomList(title: "BATTERY_HEALTH".localize(table: table)) {
-            if Int(maximumCapacity) > 79 {
+            if maximumCapacity > 79 {
                 Section {} footer: {
                     Text(.init("ABOUT_BATTERIES_TEXT_IPHONE".localize(table: table, "[\("ABOUT_BATTERIES_LINK".localize(table: table))](\("BW_LM_URL_3".localize(table: table)))")))
                 }
@@ -38,16 +34,7 @@ struct BatteryHealthChargingView: View {
             }
             
             Section {
-                LabeledContent("MAXIMUM_CAPACITY_NAME".localize(table: table), value: "\(maximumCapacity == -1 ? "Unknown" : String(maximumCapacity))%")
-                    .onAppear {
-                        // Get battery capacity value
-                        let answer = MGHelper.read(key: "f2DlVMUVcV+MeWs/g2ku+g") ?? "-1"
-                        if answer == "101" {
-                            maximumCapacity = 100
-                        } else if answer != "-1" {
-                            maximumCapacity = Int(answer)!
-                        }
-                    }
+                LabeledContent("MAXIMUM_CAPACITY_NAME".localize(table: table), value: "\(maximumCapacity)%")
             } footer: {
                 Text("MAXIMUM_CAPACITY_FOOTER_TEXT", tableName: table)
             }
@@ -65,18 +52,7 @@ struct BatteryHealthChargingView: View {
                         temporaryOptimizePauseEnabled = false
                     }
             } footer: {
-                Text("SC_FOOTER_TEXT".localize(table: table, "80%") + " \(temporaryOptimizePauseEnabled ? "SC_FOOTER_TEXT_TEMP_DISABLE_ADDITION".localize(table: table, "6:00 AM") : String())")
-            }
-            .alert("SC_ALERT_TITLE".localize(table: table), isPresented: $showingOptimizeWarning) {
-                Button("SC_ALERT_TEMP_DISABLE".localize(table: table), role: .none) {
-                    temporaryOptimizePauseEnabled = true
-                }
-                Button("SC_ALERT_DISABLE".localize(table: table), role: .none) {
-                    temporaryOptimizePauseEnabled = false
-                }
-                Button("SC_ALERT_LEAVE_ON".localize(table: table), role: .none) {
-                    optimizedBatteryChargingEnabled = true
-                }
+                Text("\("SC_FOOTER_TEXT".localize(table: table, "80%")) \(temporaryOptimizePauseEnabled ? "SC_FOOTER_TEXT_TEMP_DISABLE_ADDITION".localize(table: table, "6:00 AM") : "")")
             }
             
             Section {
@@ -86,18 +62,29 @@ struct BatteryHealthChargingView: View {
                         temporaryCleanPauseEnabled = false
                     }
             } footer: {
-                Text("CLEAN_ENERGY_FOOTER".localize(table: table) + " \(temporaryCleanPauseEnabled ? "SC_FOOTER_TEXT_TEMP_DISABLE_ADDITION".localize(table: table, "6:00 AM") : String())")
+                Text("\("CLEAN_ENERGY_FOOTER".localize(table: table)) \(temporaryCleanPauseEnabled ? "SC_FOOTER_TEXT_TEMP_DISABLE_ADDITION".localize(table: table, "6:00 AM") : "")")
             }
-            .alert("CEC_ALERT_TITLE".localize(table: table), isPresented: $showingCleanEnergyWarning) {
-                Button("CEC_ALERT_TEMP_DISABLE".localize(table: table), role: .none) {
-                    temporaryCleanPauseEnabled = true
-                }
-                Button("CEC_ALERT_DISABLE".localize(table: table), role: .none) {
-                    cleanEnergyChargingEnabled = false
-                }
-                Button("CEC_ALERT_LEAVE_ON".localize(table: table), role: .none) {
-                    cleanEnergyChargingEnabled = true
-                }
+        }
+        .alert("SC_ALERT_TITLE".localize(table: table), isPresented: $showingOptimizeWarning) {
+            Button("SC_ALERT_TEMP_DISABLE".localize(table: table), role: .none) {
+                temporaryOptimizePauseEnabled = true
+            }
+            Button("SC_ALERT_DISABLE".localize(table: table), role: .none) {
+                temporaryOptimizePauseEnabled = false
+            }
+            Button("SC_ALERT_LEAVE_ON".localize(table: table), role: .none) {
+                optimizedBatteryChargingEnabled = true
+            }
+        }
+        .alert("CEC_ALERT_TITLE".localize(table: table), isPresented: $showingCleanEnergyWarning) {
+            Button("CEC_ALERT_TEMP_DISABLE".localize(table: table), role: .none) {
+                temporaryCleanPauseEnabled = true
+            }
+            Button("CEC_ALERT_DISABLE".localize(table: table), role: .none) {
+                cleanEnergyChargingEnabled = false
+            }
+            Button("CEC_ALERT_LEAVE_ON".localize(table: table), role: .none) {
+                cleanEnergyChargingEnabled = true
             }
         }
     }
