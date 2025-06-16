07c7486886469d17b3af30bac9cf4f78159b5b3a
Chris <zhris@zhr.is>
2025-06-16 17:42:04 -0400

    Update OtherNetworkView to add animated icon and header

--- Preferences/Views/Radios/Network/OtherNetworkView.swift
+++ Preferences/Views/Radios/Network/OtherNetworkView.swift
@@ -10,17 +10,52 @@ import SwiftUI
 struct OtherNetworkView: View {
     @Environment(\.dismiss) private var dismiss
     @FocusState private var networkFocused: Bool
-    @State private var animate = true
+    @State private var animatingSymbol = true
     @State private var networkName = ""
     @State private var security = "kWFLocSecurityWPA2WPA3Title"
     @State private var username = ""
     @State private var password = ""
     @State var status = "kWFLocOtherNetworksPrompt"
+    @State private var frameY = 100.0
     let table = "WiFiKitUILocalizableStrings"
-    
+
     var body: some View {
         NavigationStack {
             List {
+                // Wi-Fi symbol
+                Image(systemName: "wifi")
+                    .font(.system(size: 64))
+                    .foregroundStyle(.blue.gradient)
+                    .symbolEffect(.drawOn, isActive: animatingSymbol)
+                    .onAppear {
+                        Task {
+                            try await Task.sleep(for: .seconds(0.5))
+                            animatingSymbol = false
+                        }
+                    }
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .listRowBackground(Color.clear)
+
+                // Join Wi-Fi Network
+                Section {
+                    Text("Join Wi-Fi Network")
+                        .padding(.horizontal, -15)
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .opacity(frameY >= 100.0 ? 1.0 : 0.0)
+                        .overlay {
+                            GeometryReader { geo in
+                                Color.clear
+                                    .onChange(of: geo.frame(in: .scrollView).minY) {
+                                        withAnimation {
+                                            frameY = geo.frame(in: .scrollView).minY
+                                        }
+                                    }
+                            }
+                        }
+                }
+                .listRowBackground(Color.clear)
+
                 // Name
                 Section {
                     HStack {
@@ -53,7 +88,8 @@ struct OtherNetworkView: View {
                     }
                 }
             }
-            .navigationTitle("kWFLocOtherNetworksTitle".localize(table: table))
+            .contentMargins(30, for: .scrollContent)
+            .navigationTitle(frameY < 100.0 ? "Join Wi-Fi Network" : "")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 ToolbarItem(placement: .topBarLeading) {
@@ -80,4 +116,3 @@ struct OtherNetworkView: View {
         OtherNetworkView()
     }
 }
-
