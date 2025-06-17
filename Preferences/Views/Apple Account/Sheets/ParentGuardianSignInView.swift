b100616a48dc75a58f0cd205c48a43b22c2caa85
Chris <zhris@zhr.is>
2025-06-17 16:04:36 -0400

    Update ParentGuardianSignInView

--- Preferences/Views/Apple Account/Sheets/ParentGuardianSignInView.swift
+++ Preferences/Views/Apple Account/Sheets/ParentGuardianSignInView.swift
@@ -21,31 +21,24 @@ struct ParentGuardianSignInView: View {
         GeometryReader { geo in
             List {
                 Section {
-                    VStack(alignment: .center, spacing: 15) {
+                    VStack(alignment: .leading, spacing: 15) {
                         Image(systemName: "figure.and.child.holdinghands")
                             .foregroundStyle(.blue)
                             .font(.system(size: 64))
+                            .frame(maxWidth: .infinity)
                         Text("PARENT_SIGN_IN_TITLE".localize(table: setupTable))
                             .fixedSize(horizontal: false, vertical: true)
-                            .font(.title)
+                            .font(.title2)
                             .fontWeight(.bold)
                         Text("PARENT_SIGN_IN_REASON_REBRAND".localize(table: setupTable, "12"))
-                    }
-                    .padding(.horizontal, 10)
-                    .multilineTextAlignment(.center)
-                }
-                .listRowBackground(Color.clear)
-                .frame(maxWidth: .infinity)
-                
-                Section {
-                    VStack(alignment: .center) {
+                            .font(.title2)
+                            .foregroundStyle(.secondary)
                         TextField("LOGIN_FORM_TEXTFIELD_NAME".localize(table: setupTable), text: $username)
                             .usernameTextStyle()
-                        Spacer()
                     }
+                    .padding(.horizontal, 10)
                 }
                 .listRowBackground(Color.clear)
-                .listRowSeparator(.hidden)
                 
                 Section {
                     VStack {
@@ -54,26 +47,22 @@ struct ParentGuardianSignInView: View {
                         }
                         // Privacy Button
                         OBPrivacyLinkView(bundleIdentifiers: ["com.apple.onboarding.appleid"])
-                            .frame(minHeight: 100)
-                        
-                        Button {
-                            signingIn.toggle()
-                            showingAlert.toggle()
-                        } label: {
-                            if signingIn || showingOptionsAlert {
-                                ContinueButton(username: $username, loading: true)
-                            } else {
-                                ContinueButton(username: $username)
-                            }
-                        }
-                        .frame(height: 50)
-                        .disabled(username.count < 1)
-                        .alert("VERIFICATION_FAILED_TITLE".localize(table: UITable), isPresented: $showingAlert) {
-                            Button("SETUP_VIEW_BUTTON_OK".localize(table: setupTable)) {
+                            .frame(minHeight: 140)
+
+                        if signingIn || showingOptionsAlert {
+                            Button {
                                 signingIn.toggle()
+                                showingAlert.toggle()
+                            } label: {
+                                ContinueButton(username: $username, loading: true)
                             }
-                        } message: {
-                            Text("BAD_NETWORK_ALERT_MESSAGE_REBRAND".localize(table: UITable))
+                            .frame(height: 50)
+                        } else {
+                            OBBoldTrayButton("SIGN_IN_BUTTON_CONTINUE".localize(table: UITable)) {
+                                signingIn.toggle()
+                                showingAlert.toggle()
+                            }
+                            .disabled(username.count < 1)
                         }
                     }
                 }
@@ -81,6 +70,13 @@ struct ParentGuardianSignInView: View {
             }
             .background(colorScheme == .light ? .white : Color(UIColor.systemBackground))
             .scrollContentBackground(.hidden)
+            .alert("VERIFICATION_FAILED_TITLE".localize(table: UITable), isPresented: $showingAlert) {
+                Button("SETUP_VIEW_BUTTON_OK".localize(table: setupTable)) {
+                    signingIn.toggle()
+                }
+            } message: {
+                Text("BAD_NETWORK_ALERT_MESSAGE_REBRAND".localize(table: UITable))
+            }
         }
     }
 }
