1c3fcad35f6fa2efbd8a4062a69c06b01a36ad97
zhrispineda <zhris@zhr.is>
2025-07-03 09:09:37 -0400

    Update DisplayView & StandByView and delete AmbientSettings string catalog

--- Preferences/Views/Main/StandBy/DisplayView.swift
+++ Preferences/Views/Main/StandBy/DisplayView.swift
@@ -8,46 +8,47 @@
 import SwiftUI
 
 struct DisplayView: View {
-    // Variables
     @AppStorage("AmbientSelectedSetting") private var selected = "ALWAYS_ON_DISPLAY_TURN_OFF_AUTOMATICALLY"
     @AppStorage("AmbientEnabledSetting") private var nightModeEnabled = true
     @AppStorage("AmbientMotionSetting") private var motionWakeEnabled = true
     let options = ["ALWAYS_ON_DISPLAY_TURN_OFF_AUTOMATICALLY", "ALWAYS_ON_DISPLAY_TURN_OFF_AFTER_IDLE", "ALWAYS_ON_DISPLAY_TURN_OFF_NEVER"]
+    let path = "/System/Library/PreferenceBundles/AmbientSettings.bundle"
     let table = "AmbientSettings"
     
     var body: some View {
-        CustomList(title: "ALWAYS_ON_DISPLAY_OPTIONS".localize(table: table), topPadding: true) {
+        CustomList(title: "ALWAYS_ON_DISPLAY_OPTIONS".localized(path: path, table: table), topPadding: true) {
             Section {
                 Picker("", selection: $selected) {
                     ForEach(options, id: \.self) { option in
-                        Text(option.localize(table: table))
+                        Text(option.localized(path: path, table: table))
                     }
                 }
                 .pickerStyle(.inline)
                 .labelsHidden()
             } header: {
-                Text("TURN_DISPLAY_OFF_GROUP_HEADER".localize(table: table))
+                Text("TURN_DISPLAY_OFF_GROUP_HEADER".localized(path: path, table: table))
             } footer: {
-                Text("ALWAYS_ON_DISPLAY_TURN_OFF_FOOTER_AUTOMATICALLY".localize(table: table))
+                Text("ALWAYS_ON_DISPLAY_TURN_OFF_FOOTER_AUTOMATICALLY".localized(path: path, table: table))
             }
             
             Section {
-                Toggle("NIGHT_MODE_ENABLED".localize(table: table), isOn: $nightModeEnabled)
+                Toggle("NIGHT_MODE_ENABLED".localized(path: path, table: table), isOn: $nightModeEnabled)
                     .disabled(selected == "ALWAYS_ON_DISPLAY_TURN_OFF_NEVER")
             } header: {
-                Text("AT_NIGHT_GROUP_HEADER".localize(table: table))
+                Text("AT_NIGHT_GROUP_HEADER".localized(path: path, table: table))
             } footer: {
-                Text("NIGHT_MODE_ENABLED_FOOTER".localize(table: table, "AMBIENT_FEATURE_NAME"))
+                Text("NIGHT_MODE_ENABLED_FOOTER".localized(path: path, table: table, "AMBIENT_FEATURE_NAME".localized(path: path, table: table)))
             }
             
             if selected == "ALWAYS_ON_DISPLAY_TURN_OFF_AUTOMATICALLY" {
                 Section {
-                    Toggle("MOTION_TO_WAKE_ENABLED".localize(table: table), isOn: $motionWakeEnabled)
+                    Toggle("MOTION_TO_WAKE_ENABLED".localized(path: path, table: table), isOn: $motionWakeEnabled)
                 } footer: {
-                    Text("MOTION_TO_WAKE_ENABLED_FOOTER".localize(table: table, "AMBIENT_FEATURE_NAME"))
+                    Text("MOTION_TO_WAKE_ENABLED_FOOTER".localized(path: path, table: table, "AMBIENT_FEATURE_NAME".localized(path: path, table: table)))
                 }
             }
         }
+        .animation(.default, value: selected)
     }
 }
 
--- Preferences/Views/Main/StandByView.swift
+++ Preferences/Views/Main/StandByView.swift
@@ -8,51 +8,53 @@
 import SwiftUI
 
 struct StandByView: View {
-    // Variables
     @AppStorage("AmbientEnabledSetting") private var standByEnabled = true
     @AppStorage("AmbientNightSetting") private var nightModeEnabled = true
     @AppStorage("AmbientNotificationSetting") private var showNotificationsEnabled = true
     @AppStorage("AmbientPreviewSetting") private var showPreviewsTapOnlyEnabled = false
+    let path = "/System/Library/PreferenceBundles/AmbientSettings.bundle"
     let table = "AmbientSettings"
     
     var body: some View {
-        CustomList(title: "AMBIENT_FEATURE_NAME".localize(table: table)) {
+        CustomList(title: "AMBIENT_FEATURE_NAME".localized(path: path, table: table)) {
             Section {
-                Toggle("AMBIENT_FEATURE_NAME".localize(table: table), isOn: $standByEnabled.animation())
+                Toggle("AMBIENT_FEATURE_NAME".localized(path: path, table: table), isOn: $standByEnabled.animation())
             } footer: {
-                Text("AMBIENT_MODE_ENABLED_FOOTER".localize(table: table, "AMBIENT_FEATURE_NAME"))
+                Text("AMBIENT_MODE_ENABLED_FOOTER".localized(path: path, table: table, "AMBIENT_FEATURE_NAME".localized(path: path, table: table)))
             }
             
             if standByEnabled {
                 if UIDevice.AlwaysOnDisplayCapability {
-                    NavigationLink("ALWAYS_ON_DISPLAY_OPTIONS".localize(table: table), destination: DisplayView())
+                    NavigationLink("ALWAYS_ON_DISPLAY_OPTIONS".localized(path: path, table: table), destination: DisplayView())
                 } else {
                     Section {
-                        Toggle("NIGHT_MODE_ENABLED".localize(table: table), isOn: $nightModeEnabled)
+                        Toggle("NIGHT_MODE_ENABLED".localized(path: path, table: table), isOn: $nightModeEnabled)
                     } header: {
-                        Text("DISPLAY_SETTINGS_GROUP_HEADER", tableName: table)
+                        Text("DISPLAY_SETTINGS_GROUP_HEADER".localized(path: path, table: table))
                     } footer: {
-                        Text("NIGHT_MODE_ENABLED_FOOTER".localize(table: table, "AMBIENT_FEATURE_NAME"))
+                        Text("NIGHT_MODE_ENABLED_FOOTER".localized(path: path, table: table))
                     }
                 }
                 
                 Section {
-                    Toggle("NOTIFICATIONS_ENABLED".localize(table: table), isOn: $showNotificationsEnabled.animation())
+                    Toggle("NOTIFICATIONS_ENABLED".localized(path: path, table: table), isOn: $showNotificationsEnabled.animation())
                 } header: {
-                    Text("NOTIFICATIONS_GROUP_HEADER", tableName: table)
+                    Text("NOTIFICATIONS_GROUP_HEADER".localized(path: path, table: table))
                 } footer: {
-                    Text("NOTIFICATIONS_ENABLED_FOOTER".localize(table: table, "AMBIENT_FEATURE_NAME"))
+                    Text("NOTIFICATIONS_ENABLED_FOOTER".localized(path: path, table: table, "AMBIENT_FEATURE_NAME".localized(path: path, table: table)))
                 }
                 
                 if showNotificationsEnabled {
                     Section {
-                        Toggle("NOTIFICATIONS_PREVIEW".localize(table: table), isOn: $showPreviewsTapOnlyEnabled)
+                        Toggle("NOTIFICATIONS_PREVIEW".localized(path: path, table: table), isOn: $showPreviewsTapOnlyEnabled)
                     } footer: {
-                        Text("NOTIFICATIONS_PREVIEW_FOOTER".localize(table: table, "AMBIENT_FEATURE_NAME"))
+                        Text("NOTIFICATIONS_PREVIEW_FOOTER".localized(path: path, table: table, "AMBIENT_FEATURE_NAME".localized(path: path, table: table)))
                     }
                 }
             }
         }
+        .animation(.default, value: standByEnabled)
+        .animation(.default, value: showNotificationsEnabled)
     }
 }
 
