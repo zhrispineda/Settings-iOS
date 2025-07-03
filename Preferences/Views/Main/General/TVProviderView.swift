12ae279782504a23dd97a4dce7e68e236b587dc6
zhrispineda <zhris@zhr.is>
2025-07-03 10:57:04 -0400

    Update TVProviderView and delete VideoSubscriberAccount string catalog

--- Preferences/Views/Main/General/TVProviderView.swift
+++ Preferences/Views/Main/General/TVProviderView.swift
@@ -8,31 +8,34 @@
 import SwiftUI
 
 struct TVProviderView: View {
-    // Variables
-    @State private var searchable = String()
+    @State private var showingSheet = false
+    @State private var searchable = ""
     let providers = ["AT&T U-verse", "CenturyLink Prism", "Cox", "DIRECTV", "DIRECTV STREAM", "DISH", "Frontier", "Hulu", "Mediacom", "Optimum", "Optimum TV", "Sling TV", "Spectrum", "Verizon Fios", "Xfinity"]
     let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ#".map(String.init)
-    let table = "VideoSubscriberAccount"
+    let path = "/System/Library/Frameworks/VideoSubscriberAccount.framework"
+    let privacyPath = "/System/Library/OnBoardingBundles/com.apple.onboarding.tvprovider.bundle"
     
     var body: some View {
         ZStack {
-            CustomList(title: "TV_PROVIDER_TITLE".localize(table: table), topPadding: true) {
+            CustomList(title: "TV_PROVIDER_TITLE".localized(path: path), topPadding: true) {
                 Section {
                     ForEach(providers, id: \.self) { provider in
                         Button(provider) {}
                             .foregroundStyle(.primary)
                     }
                 } header: {
-                    Text("IDENTITY_PROVIDER_PICKER_HEADER", tableName: table).textCase(nil)
+                    Text("IDENTITY_PROVIDER_PICKER_HEADER".localized(path: path)).textCase(nil)
                 }
                 
-                // TODO: Remaining TV Providers
-                
                 Section {
-                    Button("REGIONS_PICKER_OTHER_REGIONS_ROW_TITLE".localize(table: table)) {}
+                    Button("REGIONS_PICKER_OTHER_REGIONS_ROW_TITLE".localized(path: path)) {}
                         .foregroundStyle(.primary)
                 } footer: {
-                    Text("\("IDENTITY_PROVIDER_PICKER_SETTINGS_FOOTER".localize(table: table))\n\n[\("BUTTON_TITLE".localize(table: "About TV Provider & Privacy…"))](#)")
+                    Text("IDENTITY_PROVIDER_PICKER_SETTINGS_FOOTER".localized(path: path))
+                }
+                
+                Section {} footer: {
+                    Text("[\("BUTTON_TITLE".localized(path: privacyPath, table: "TVProvider"))](pref://)\n")
                 }
             }
             .searchable(text: $searchable, placement: .toolbar)
@@ -49,6 +52,15 @@ struct TVProviderView: View {
             .font(.system(size: 11))
             .frame(maxWidth: .infinity, alignment: .trailing)
             .padding(.trailing, 1)
+        }
+        .onOpenURL { url in
+            if url.absoluteString == "pref://" {
+                showingSheet = true
+            }
+        }
+        .sheet(isPresented: $showingSheet) {
+            OnBoardingKitView(bundleID: "com.apple.onboarding.tvprovider")
+                .ignoresSafeArea()
         }
     }
 }
