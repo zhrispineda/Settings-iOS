cc652ed9ae94a27024993432df55d71de2b0a68a
zhrispineda <zhris@zhr.is>
2025-07-04 18:19:29 -0400

    Update WalletView and delete Wallet-related string catalogs

--- Preferences/Views/Services/WalletView.swift
+++ Preferences/Views/Services/WalletView.swift
@@ -8,121 +8,69 @@
 import SwiftUI
 
 struct WalletView: View {
-    @State private var cellularEnabled = true
     @AppStorage("AppleCashEnabled") private var appleCashEnabled = false
-    @State private var doubleClickButtonEnabled = true
+    @State private var cellularEnabled = true
     @State private var hideExpiredPassesEnabled = true
-    @AppStorage("PayLaterEnabled") private var payLaterEnabled = true
-    @AppStorage("RewardsEnabled") private var rewardsEnabled = true
-    @AppStorage("CompatibleCardsEnabled") private var compatibleCardsEnabled = false
-    @State private var addOrdersWalletEnabled = true
-    @State private var orderNotificationsEnabled = true
-    @AppStorage("OtherPayLaterEnabled") private var otherPayLaterOptionsEnabled = true
     let path = "/System/Library/Frameworks/PassKit.framework/"
-    let nfcTable = "ContactlessAndCredentialSettings_Localizable"
-    let offerTable = "PaymentOffers_Localizable"
     let payTable = "Payment_Localizable"
     let peerTable = "PeerPayment_Localizable"
-    let virtualTable = "VirtualCard_Localizable"
     let walletTable = "WalletSettings_Localizable"
     
     var body: some View {
         CustomList(title: "WALLET_&_APPLE_PAY".localized(path: path, table: walletTable), topPadding: true) {
             if UIDevice.IsSimulator {
-                PermissionsView(appName: "PASS_DETAILS_WALLET".localize(table: payTable), cellular: false, location: false, cellularEnabled: $cellularEnabled)
+                PermissionsView(appName: "WALLET_&_APPLE_PAY".localized(path: path, table: walletTable), cellular: false, liveActivityToggle: true, location: false, siri: true, cellularEnabled: $cellularEnabled)
             } else {
-                PermissionsView(appName: "WALLET_&_APPLE_PAY".localized(path: path, table: walletTable), cellular: false, location: false, siri: false, cellularEnabled: $cellularEnabled)
+                PermissionsView(appName: "WALLET_&_APPLE_PAY".localized(path: path, table: walletTable), cellular: true, liveActivityToggle: true, location: false, siri: true, cellularEnabled: $cellularEnabled)
             }
             
             if !UIDevice.IsSimulator {
                 Section {
-                    Toggle("SE_STORAGE_APPLET_CATEGORY_APLET_TYPE_PTC".localize(table: payTable), isOn: $appleCashEnabled.animation())
+                    SLink("SETTINGS_APPLE_PAY_DEFAULTS_TITLE".localized(path: path, table: payTable), icon: "com.apple.graphic-icon.account.payment", subtitle: "SETTINGS_APPLE_PAY_DEFAULTS_SUBTITLE".localized(path: path, table: payTable)) {}
+                }
+                
+                Section {
+                    Toggle("SE_STORAGE_APPLET_CATEGORY_APLET_TYPE_PTC".localized(path: path, table: payTable), isOn: $appleCashEnabled.animation())
                 } footer: {
                     if UIDevice.iPhone {
-                        Text("PEER_PAYMENT_REGISTRATION_FOOTER_TEXT_IPHONE", tableName: peerTable)
+                        Text("PEER_PAYMENT_REGISTRATION_FOOTER_TEXT_IPHONE".localized(path: path, table: peerTable))
                     } else if UIDevice.iPad {
-                        Text("PEER_PAYMENT_REGISTRATION_FOOTER_TEXT_IPAD", tableName: peerTable)
+                        Text("PEER_PAYMENT_REGISTRATION_FOOTER_TEXT_IPAD".localized(path: path, table: peerTable))
                     }
                 }
                 
                 Section {
-//                    if appleCashEnabled {
-//                        NavigationLink(destination: EmptyView()) {
-//                            VStack(alignment: .leading, spacing: 5) {
-//                                Text("SE_STORAGE_APPLET_CATEGORY_APLET_TYPE_PTC", tableName: payTable)
-//                                Text("PEER_PAYMENT_SETTINGS_REGISTRATION_NOT_SET_UP", tableName: peerTable)
-//                                    .font(.caption)
-//                            }
-//                        }
-//                        .alignmentGuide(.listRowSeparatorLeading) { ViewDimensions in
-//                            return -20
-//                        }
-//                    }
-                    Button("PEER_PAYMENT_ADD_CARD_BUTTON_TITLE".localize(table: peerTable)) {}
+                    Button("PEER_PAYMENT_ADD_CARD_BUTTON_TITLE".localized(path: path, table: peerTable)) {}
                 } header: {
-                    Text("SETTINGS_PAYMENT_CARDS_GROUP", tableName: payTable)
+                    Text("SETTINGS_PAYMENT_CARDS_GROUP".localized(path: path, table: payTable))
                 } footer: {
                     if !appleCashEnabled {
-                        Text(.init("\(UIDevice.PearlIDCapability ? UIDevice.iPhone ? "SETTINGS_ABOUT_FOOTER_FACEID_IPHONE".localize(table: payTable) : "SETTINGS_ABOUT_FOOTER_FACEID_IPAD".localize(table: payTable) : UIDevice.iPad ? "SETTINGS_ABOUT_FOOTER_IPAD".localize(table: payTable) : "SETTINGS_ABOUT_FOOTER_IPHONE".localize(table: payTable)) [\("APPLE_PAY_PRIVACY".localized(path: path, table: walletTable))](#)"))
-                    }
-                }
-                
-                if UIDevice.iPhone {
-                    Section {
-                        Toggle(UIDevice.HomeButtonCapability ? "DOUBLE_CLICK_HOME_BUTTON".localize(table: nfcTable) : "DOUBLE_CLICK_SIDE_BUTTON".localize(table: nfcTable), isOn: $doubleClickButtonEnabled)
-                    } footer: {
-                        Text(UIDevice.HomeButtonCapability ? "DOUBLE_CLICK_HOME_BUTTON_FOOTER" : "DOUBLE_CLICK_SIDE_BUTTON_FOOTER", tableName: nfcTable)
+                        Text(.init("\(UIDevice.PearlIDCapability ? UIDevice.iPhone ? "SETTINGS_ABOUT_FOOTER_FACEID_IPHONE".localized(path: path, table: payTable) : "SETTINGS_ABOUT_FOOTER_FACEID_IPAD".localized(path: path, table: payTable) : UIDevice.iPad ? "SETTINGS_ABOUT_FOOTER_IPAD".localized(path: path, table: payTable) : "SETTINGS_ABOUT_FOOTER_IPHONE".localized(path: path, table: payTable)) [\("APPLE_PAY_PRIVACY".localized(path: path, table: walletTable))](#)"))
                     }
                 }
             }
+            
+            SLink("FPAN_ADD_TO_APPLE_PAY_TITLE".localized(path: path, table: payTable), icon: "None", status: "0") {}
             
             if !UIDevice.IsSimulator && UIDevice.iPhone {
                 Section {
-                    SettingsLink("SETTINGS_EXPRESS_TRANSIT_CARDS_SECTION_HEADER".localize(table: payTable), status: "NONE".localize(table: payTable), destination: EmptyView())
+                    SettingsLink("SETTINGS_EXPRESS_TRANSIT_CARDS_SECTION_HEADER".localized(path: path, table: payTable), status: "NONE".localized(path: path, table: payTable), destination: EmptyView())
                 } header: {
-                    Text("SETTINGS_EXPRESS_TRANSIT_CARD_CATEGORY_SECTION_HEADER", tableName: payTable)
+                    Text("SETTINGS_EXPRESS_TRANSIT_CARD_CATEGORY_SECTION_HEADER".localized(path: path, table: payTable))
                 } footer: {
-                    Text(UIDevice.PearlIDCapability ? "SETTINGS_EXPRESS_TRANSIT_SECTION_FOOTER_IPHONE_LPEM_FACEID" : "SETTINGS_EXPRESS_TRANSIT_SECTION_FOOTER_IPHONE_LPEM_TOUCHID", tableName: payTable)
-                }
-                
-                if appleCashEnabled {
-                    Section {
-                        NavigationLink("SETTINGS_TRANSACTION_DEFAULTS_PAYMENT_CARD".localize(table: payTable)) {}
-                            .disabled(true)
-                        NavigationLink("SETTINGS_OPTIONS_SHIPPING_ADDRESS_VC_TITLE".localize(table: payTable)) {}
-                        NavigationLink("SETTINGS_TRANSACTION_DEFAULTS_EMAIL".localize(table: payTable)) {}
-                        NavigationLink("SETTINGS_TRANSACTION_DEFAULTS_PHONE".localize(table: payTable)) {}
-                    } header: {
-                        Text("SETTINGS_TRANSACTION_DEFAULTS_GROUP", tableName: payTable)
-                    } footer: {
-                        Text("\("SETTINGS_TRANSACTION_DEFAULTS_FOOTER".localize(table: payTable)) [\("APPLE_PAY_PRIVACY".localized(path: path, table: walletTable))](#)")
-                    }
+                    Text(UIDevice.PearlIDCapability ? "SETTINGS_EXPRESS_TRANSIT_SECTION_FOOTER_IPHONE_LPEM_FACEID".localized(path: path, table: payTable) : "SETTINGS_EXPRESS_TRANSIT_SECTION_FOOTER_IPHONE_LPEM_TOUCHID".localized(path: path, table: payTable))
                 }
             }
             
             Section {
-                Toggle("PAY_LATER_BADGE_TEXT".localize(table: offerTable), isOn: $payLaterEnabled)
-                Toggle("Rewards", isOn: $rewardsEnabled)
-            } header: {
-                Text("CARD_BENEFITS_HEADER", tableName: offerTable)
-            } footer: {
-                Text("CARD_BENEFITS_FOOTER", tableName: offerTable)
+                Toggle("ALLOW_EXPIRED_PASSES_TITLE".localized(path: path), isOn: $hideExpiredPassesEnabled)
             }
             
             Section {
-                Toggle("Other Pay Later Options", isOn: $otherPayLaterOptionsEnabled)
-            } footer: {
-                Text("See additional pay later services not associated with your cards in Wallet when you check out with Apple Pay.")
-            }
-            
-            Section {
-                Toggle("ALLOW_ONLINE_PAYMENTS_TITLE".localize(table: virtualTable), isOn: $compatibleCardsEnabled)
-            } header: {
-                Text("ALLOW_ONLINE_PAYMENTS_HEADER", tableName: virtualTable)
-            } footer: {
-                Text("ALLOW_ONLINE_PAYMENTS_FOOTER", tableName: virtualTable)
+                NavigationLink("Order Tracking") {}
             }
         }
+        .animation(.default, value: appleCashEnabled)
     }
 }
 
