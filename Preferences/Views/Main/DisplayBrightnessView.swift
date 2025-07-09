3e4e9665dc4dec7449ef57f9e435a586aabde9dc
zhrispineda <zhris@zhr.is>
2025-07-09 10:28:14 -0400

    Update DisplayBrightnessView and delete Display string catalog

--- Preferences/Views/Main/DisplayBrightnessView.swift
+++ Preferences/Views/Main/DisplayBrightnessView.swift
@@ -20,6 +20,7 @@ struct DisplayBrightnessView: View {
     @State private var appearance: Theme = .dark
     @State private var brightness = UIScreen.main.brightness
     @State private var lowPowerMode = false
+    let path = "/System/Library/PrivateFrameworks/Settings/DisplayAndBrightnessSettings.framework"
     let table = "Display"
     let phoneOptions = ["30 seconds", "1 minute", "2 minutes", "3 minutes", "4 minutes", "5 minutes", "NEVER"]
     let tabletOptions = ["2 minutes", "5 minutes", "10 minutes", "15 minutes", "NEVER"]
@@ -30,7 +31,7 @@ struct DisplayBrightnessView: View {
     }
     
     var body: some View {
-        CustomList(title: "DISPLAY_AND_BRIGHTNESS".localize(table: table), topPadding: true) {
+        CustomList(title: "DISPLAY_AND_BRIGHTNESS".localized(path: path, table: table), topPadding: true) {
             // MARK: Appearance
             Section {
                 HStack {
@@ -55,7 +56,7 @@ struct DisplayBrightnessView: View {
                                     .foregroundStyle(.white)
                                     .padding(.bottom, UIDevice.iPhone ? 70 : 80)
                             }
-                            Text("COMPATIBLE_APPEARANCE_CHOICE_LIGHT", tableName: table)
+                            Text("COMPATIBLE_APPEARANCE_CHOICE_LIGHT".localized(path: path, table: table))
                                 .font(.subheadline)
                                 .padding(.bottom, -5)
                             Image(systemName: appearance == .light ? "checkmark.circle.fill": "circle")
@@ -84,7 +85,7 @@ struct DisplayBrightnessView: View {
                                     .foregroundStyle(.white)
                                     .padding(.bottom, UIDevice.iPhone ? 70 : 80)
                             }
-                            Text("COMPATIBLE_APPEARANCE_CHOICE_DARK", tableName: table)
+                            Text("COMPATIBLE_APPEARANCE_CHOICE_DARK".localized(path: path, table: table))
                                 .font(.subheadline)
                                 .padding(.bottom, -5)
                             Image(systemName: appearance == .dark ? "checkmark.circle.fill": "circle")
@@ -100,19 +101,19 @@ struct DisplayBrightnessView: View {
                     lowPowerMode = ProcessInfo.processInfo.isLowPowerModeEnabled
                 }
                 
-                Toggle("AUTOMATIC".localize(table: table), isOn: $automaticEnabled.animation())
+                Toggle("AUTOMATIC".localized(path: path, table: table), isOn: $automaticEnabled.animation())
                 
                 if automaticEnabled {
-                    SettingsLink("APPEARANCE_OPTIONS".localize(table: table), status: "LIGHT_UNTIL_SUNSET".localize(table: table), destination: EmptyView())
+                    SettingsLink("APPEARANCE_OPTIONS".localized(path: path, table: table), status: "LIGHT_UNTIL_SUNSET".localized(path: path, table: table), destination: EmptyView())
                 }
             } header: {
-                Text("APPEARANCE", tableName: table)
+                Text("APPEARANCE".localized(path: path, table: table))
             }
             
             // MARK: Text
             Section {
-                NavigationLink("TEXT_SIZE".localize(table: table), destination: BundleControllerView("/System/Library/PrivateFrameworks/Settings/DisplayAndBrightnessSettings.framework/DisplayAndBrightnessSettings", controller: "DBSLargeTextController", title: "TEXT_SIZE", table: table))
-                Toggle("BOLD_TEXT".localize(table: table), isOn: $boldTextEnabled)
+                NavigationLink("TEXT_SIZE".localized(path: path, table: table), destination: BundleControllerView("/System/Library/PrivateFrameworks/Settings/DisplayAndBrightnessSettings.framework/DisplayAndBrightnessSettings", controller: "DBSLargeTextController", title: "TEXT_SIZE", table: table))
+                Toggle("BOLD_TEXT".localized(path: path, table: table), isOn: $boldTextEnabled)
             }
             
             // MARK: Brightness
@@ -122,7 +123,7 @@ struct DisplayBrightnessView: View {
                            in: 0.0...1.0,
                            minimumValueLabel: Image(systemName: "sun.min.fill"),
                            maximumValueLabel: Image(systemName: "sun.max.fill"),
-                           label: { Text("BRIGHTNESS", tableName: table)  }
+                           label: { Text("BRIGHTNESS".localized(path: path, table: table))  }
                     )
                 }
                 .foregroundStyle(.secondary)
@@ -131,37 +132,52 @@ struct DisplayBrightnessView: View {
                     UIScreen.main.brightness = brightness
                 }
                 
-                Toggle("WHITE_BALANCE".localize(table: table), isOn: $trueToneEnabled)
+                Toggle("WHITE_BALANCE".localized(path: path, table: table), isOn: $trueToneEnabled)
             } header: {
-                Text("Brightness", tableName: table)
+                Text("BRIGHTNESS".localized(path: path, table: table))
             } footer: {
-                Text("WHITE_BALANCE_FOOTER", tableName: table)
+                Text("WHITE_BALANCE_FOOTER".localized(path: path, table: table))
             }
             
             // MARK: Night Shift
             Section {
-                SettingsLink("BLUE_LIGHT_REDUCTION".localize(table: table), status: "OFF".localize(table: table), destination: EmptyView())
+                SettingsLink("BLUE_LIGHT_REDUCTION".localized(path: path, table: table), status: "OFF".localized(path: path, table: table), destination: EmptyView())
             }
             
             // MARK: Auto-Lock
             Section {
-                SettingsLink("AUTOLOCK".localize(table: table), status: autoLockDuration.localize(table: table), destination: SelectOptionList("AUTOLOCK", options: UIDevice.iPhone ? phoneOptions : tabletOptions, selectedBinding: $autoLockDuration, table: "Display"))
-                    .disabled(lowPowerMode)
+                SettingsLink(
+                    "AUTOLOCK".localized(
+                        path: path,
+                        table: table
+                    ),
+                    status: autoLockDuration.localized(
+                        path: path,
+                        table: table
+                    ),
+                    destination: SelectOptionList(
+                        "AUTOLOCK",
+                        options: UIDevice.iPhone ? phoneOptions : tabletOptions,
+                        selectedBinding: $autoLockDuration,
+                        table: "Display"
+                    )
+                )
+                .disabled(lowPowerMode)
                 // Raise to Wake
                 if UIDevice.iPhone {
-                    Toggle("RAISE_TO_WAKE".localize(table: table), isOn: $raiseToWakeEnabled)
+                    Toggle("RAISE_TO_WAKE".localized(path: path, table: table), isOn: $raiseToWakeEnabled)
                 }
                 // Lock / Unlock
                 if UIDevice.HallEffectCapability {
-                    Toggle("SMART_CASE_LOCK".localize(table: table), isOn: $smartCoverEnabled)
+                    Toggle("SMART_CASE_LOCK".localized(path: path, table: table), isOn: $smartCoverEnabled)
                 }
             } footer: {
                 VStack(alignment: .leading) {
                     if UIDevice.HallEffectCapability {
-                        Text(lowPowerMode ? "SMART_CASE_LOCK_FOOTER".localize(table: table) + "\n" : "SMART_CASE_LOCK_FOOTER".localize(table: table))
+                        Text(lowPowerMode ? "SMART_CASE_LOCK_FOOTER".localized(path: path, table: table) + "\n" : "SMART_CASE_LOCK_FOOTER".localized(path: path, table: table))
                     }
                     if lowPowerMode {
-                        Text("AUTOLOCK_LPM_FOOTER", tableName: table)
+                        Text("AUTOLOCK_LPM_FOOTER".localized(path: path, table: table))
                     }
                 }
             }
@@ -169,24 +185,24 @@ struct DisplayBrightnessView: View {
             if UIDevice.AlwaysOnDisplayCapability {
                 // MARK: Always-On
                 Section {
-                    SettingsLink("ALWAYS_ON_DISPLAY".localize(table: table), status: "ALWAYS_ON_ENABLED".localize(table: table), destination: EmptyView())
+                    SettingsLink("ALWAYS_ON_DISPLAY".localized(path: path, table: table), status: "ALWAYS_ON_ENABLED".localized(path: path, table: table), destination: EmptyView())
                 } footer: {
-                    Text("ALWAYS_ON_DESCRIPTION", tableName: table)
+                    Text("ALWAYS_ON_DESCRIPTION".localized(path: path, table: table))
                 }
             }
             
             // MARK: Display
             Section {
-                SettingsLink("VIEW".localize(table: table), status: "DEFAULT".localize(table: "Accessibility"), destination: EmptyView())
+                SettingsLink("VIEW".localized(path: path, table: table), status: "DEFAULT".localize(table: "Accessibility"), destination: EmptyView())
             } header: {
                 Text("DISPLAY_ONLY_TITLE", tableName: "Accessibility")
             } footer: {
-                Text("DEFAULT_DISPLAY_ZOOMED_STANDARD_DESCRIPTION", tableName: table)
+                Text("DEFAULT_DISPLAY_ZOOMED_STANDARD_DESCRIPTION".localized(path: path, table: table))
             }
             
             if UIDevice.ReferenceModeCapability {
                 // MARK: Reference Mode
-                NavigationLink("ADVANCED".localize(table: table)) {}
+                NavigationLink("ADVANCED".localized(path: path, table: table)) {}
             }
         }
         .animation(.default, value: automaticEnabled)
