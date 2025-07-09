8d3cc20f21a27b8a2bf1699454d4cdd19ef9b9f9
zhrispineda <zhris@zhr.is>
2025-07-09 12:58:22 -0400

    Update AppStoreView and delete StoreSettings string catalog

--- Preferences/Views/Services/AppStoreView.swift
+++ Preferences/Views/Services/AppStoreView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct AppStoreView: View {
-    // Variables
     @State private var cellularEnabled = true
     @State private var appDownloadsEnabled = false
     @State private var appUpdatesEnabled = true
@@ -17,28 +16,29 @@ struct AppStoreView: View {
     @State private var inAppRatingsReviewEnabled = true
     @State private var offloadUnusedAppsEnabled = false
     @State private var showingSheet = false
+    let path = "/System/Library/PreferenceBundles/MobileStoreSettings.bundle"
     let table = "StoreSettings"
     
     var body: some View {
-        CustomList(title: "STORE_SETTINGS_TITLE".localize(table: table), topPadding: true) {
+        CustomList(title: "STORE_SETTINGS_TITLE".localized(path: path, table: table), topPadding: true) {
             // Allow App Store to Access
-            PermissionsView(appName: "STORE_SETTINGS_TITLE".localize(table: table), cellularEnabled: $cellularEnabled)
+            PermissionsView(appName: "STORE_SETTINGS_TITLE".localized(path: path, table: table), cellularEnabled: $cellularEnabled)
             
             // Automatic Downloads
-            Section("AUTO_DOWNLOAD_ON_CELL".localize(table: table)) {
+            Section("AUTO_DOWNLOAD_ON_CELL".localized(path: path, table: table)) {
                 Toggle(isOn: $appDownloadsEnabled) {
-                    Text("AUTO_DOWNLOAD_APPS", tableName: table)
-                    Text("AUTO_DOWNLOADS_APPS_EXPLANATION", tableName: table)
+                    Text("AUTO_DOWNLOAD_APPS".localized(path: path, table: table))
+                    Text("AUTO_DOWNLOADS_APPS_EXPLANATION".localized(path: path, table: table))
                         .font(.footnote)
                 }
                 Toggle(isOn: $appDownloadsEnabled) {
-                    Text("AUTO_DOWNLOAD_UPDATES", tableName: table)
-                    Text("AUTO_DOWNLOAD_UPDATES_EXPLANATION", tableName: table)
+                    Text("AUTO_DOWNLOAD_UPDATES".localized(path: path, table: table))
+                    Text("AUTO_DOWNLOAD_UPDATES_EXPLANATION".localized(path: path, table: table))
                         .font(.footnote)
                 }
                 Toggle(isOn: $appDownloadsEnabled) {
-                    Text("AUTO_DOWNLOAD_BACKGROUND_ASSETS", tableName: table)
-                    Text("AUTO_DOWNLOAD_BACKGROUND_ASSETS_EXPLANATION", tableName: table)
+                    Text("AUTO_DOWNLOAD_BACKGROUND_ASSETS".localized(path: path, table: table))
+                    Text("AUTO_DOWNLOAD_BACKGROUND_ASSETS_EXPLANATION".localized(path: path, table: table))
                         .font(.footnote)
                 }
             }
@@ -46,45 +46,45 @@ struct AppStoreView: View {
             // Cellular Data
             if UIDevice.CellularTelephonyCapability {
                 Section {
-                    Toggle("AUTO_DOWNLOAD_ON_CELL".localize(table: table), isOn: $automaticDownloadsEnabled)
-                    SettingsLink("AUTO_DOWNLOAD_APPS".localize(table: table), status: "ALWAYS_ALLOW".localize(table: table), destination: EmptyView())
+                    Toggle("AUTO_DOWNLOAD_ON_CELL".localized(path: path, table: table), isOn: $automaticDownloadsEnabled)
+                    SettingsLink("AUTO_DOWNLOAD_APPS".localized(path: path, table: table), status: "ALWAYS_ALLOW".localized(path: path, table: table), destination: EmptyView())
                 } header: {
-                    Text("CELLULAR_DATA_HEADER", tableName: table)
+                    Text("CELLULAR_DATA_HEADER".localized(path: path, table: table))
                 } footer: {
-                    Text("ALWAYS_ALLOW_EXPLANATION", tableName: table)
+                    Text("ALWAYS_ALLOW_EXPLANATION".localized(path: path, table: table))
                 }
                 .disabled(!cellularEnabled)
             }
             
             // Video Autoplay
             Section {
-                SettingsLink("AUTO_PLAY_VIDEO_SETTINGS_TITLE".localize(table: table), status: "AUTO_PLAY_VIDEO_MODE_ON".localize(table: table), destination: EmptyView())
+                SettingsLink("AUTO_PLAY_VIDEO_SETTINGS_TITLE".localized(path: path, table: table), status: "AUTO_PLAY_VIDEO_MODE_ON".localized(path: path, table: table), destination: EmptyView())
             } footer: {
-                Text("AUTO_PLAY_VIDEO_SETTINGS_DESCRIPTION", tableName: table)
+                Text("AUTO_PLAY_VIDEO_SETTINGS_DESCRIPTION".localized(path: path, table: table))
             }
             
             // In-App Ratings & Reviews
             Section {
-                Toggle("IN_APP_REVIEW_ON_CELL".localize(table: table), isOn: $inAppRatingsReviewEnabled)
+                Toggle("IN_APP_REVIEW_ON_CELL".localized(path: path, table: table), isOn: $inAppRatingsReviewEnabled)
             } footer: {
-                Text("IN_APP_REVIEW_ON_CELL_EXPLANATION", tableName: table)
+                Text("IN_APP_REVIEW_ON_CELL_EXPLANATION".localized(path: path, table: table))
             }
             
             // Offload Unused Apps
             Section {
-                Toggle("OFFLOAD_UNUSED_APPS_ON_CELL".localize(table: table), isOn: $offloadUnusedAppsEnabled)
+                Toggle("OFFLOAD_UNUSED_APPS_ON_CELL".localized(path: path, table: table), isOn: $offloadUnusedAppsEnabled)
             } footer: {
-                Text("OFFLOAD_UNUSED_APPS_ON_CELL_EXPLANATION", tableName: table)
+                Text("OFFLOAD_UNUSED_APPS_ON_CELL_EXPLANATION".localized(path: path, table: table))
             }
             
             // Privacy
             Section {
-                Button("PRIVACY_APP_STORE_ARCADE_CELL".localize(table: table)) {
+                Button("PRIVACY_APP_STORE_ARCADE_CELL".localized(path: path, table: table)) {
                     showingSheet.toggle()
                 }
-                Link("PRIVACY_PERSONALIZED_RECOMMENDATIONS_CELL".localize(table: table), destination: URL(string: "itms-apps:///personalizationTransparency?enabled=true")!)
+                Link("PRIVACY_PERSONALIZED_RECOMMENDATIONS_CELL".localized(path: path, table: table), destination: URL(string: "itms-apps:///personalizationTransparency?enabled=true")!)
             } header: {
-                Text("PRIVACY_HEADER", tableName: table)
+                Text("PRIVACY_HEADER".localized(path: path, table: table))
             }
         }
         .background {
