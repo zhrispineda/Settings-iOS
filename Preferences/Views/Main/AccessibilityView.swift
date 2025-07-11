fb15206aa1ec44436ac968e9ed3bf2dd7f099755
zhrispineda <zhris@zhr.is>
2025-07-10 20:19:57 -0400

    Update AccessibilityView for dynamic localization, delete related string catalogs

--- Preferences/Views/Main/AccessibilityView.swift
+++ Preferences/Views/Main/AccessibilityView.swift
@@ -8,11 +8,12 @@
 import SwiftUI
 
 struct AccessibilityView: View {
-    // Variables
     @AppStorage("SiriEnabled") private var siriEnabled = false
     @State private var opacity: Double = 0.0
     @State private var frameY: Double = 0.0
     @State private var showingHelpSheet = false
+    let path = "/System/Library/PrivateFrameworks/AccessibilityUIUtilities.framework"
+    let settings = "/System/Library/PreferenceBundles/AccessibilitySettings.bundle"
     let table = "Accessibility"
     let titleTable = "AccessibilityTitles"
     let cameraTable = "Accessibility-D93"
@@ -29,175 +30,165 @@ struct AccessibilityView: View {
             Section {
                 if !UIDevice.IsSimulator {
                     // VoiceOver
-                    SLink("VOICEOVER_TITLE".localize(table: titleTable), icon: "com.apple.graphic-icon.voice-over", status: "OFF".localize(table: table)) {
+                    SLink("VOICEOVER_TITLE".localized(path: settings, table: titleTable), icon: "com.apple.graphic-icon.voice-over", status: "OFF".localized(path: settings, table: table)) {
                         BundleControllerView("AccessibilitySettings", controller: "VoiceOverController", title: "VOICEOVER_TITLE", table: table)
                     }
                     // Zoom
-                    SLink("ZOOM_TITLE".localize(table: titleTable), icon: "com.apple.AccessibilityUIServer.zoom", status: "OFF".localize(table: table)) {
+                    SLink("ZOOM_TITLE".localized(path: settings, table: titleTable), icon: "com.apple.AccessibilityUIServer.zoom", status: "OFF".localized(path: settings, table: table)) {
                         ZoomView()
                     }
                 }
                 if UIDevice.iPad {
                     // Hover Text
-                    SLink("HOVERTEXT_TITLE".localize(table: titleTable), icon: "com.apple.AccessibilityUIServer.hovertext", status: "OFF".localize(table: table)) {
+                    SLink("HOVERTEXT_TITLE".localized(path: settings, table: titleTable), icon: "com.apple.AccessibilityUIServer.hovertext", status: "OFF".localized(path: settings, table: table)) {
                         BundleControllerView("AccessibilitySettings", controller: "HoverTextController", title: "HOVERTEXT_TITLE", table: titleTable)
                     }
                 }
                 // Display & Text Size
-                SLink("DISPLAY_AND_TEXT".localize(table: titleTable), icon: "com.apple.AccessibilityUIServer.text.size") {
+                SLink("DISPLAY_AND_TEXT".localized(path: settings, table: titleTable), icon: "com.apple.AccessibilityUIServer.text.size") {
                     BundleControllerView("AccessibilitySettings", controller: "AXDisplayController", title: "DISPLAY_AND_TEXT", table: "Accessibility")
                 }
                 // Motion
-                SLink("MOTION_TITLE".localize(table: titleTable), icon: "com.apple.AccessibilityUIServer.motion") {
+                SLink("MOTION_TITLE".localized(path: settings, table: titleTable), icon: "com.apple.AccessibilityUIServer.motion") {
                     BundleControllerView("AccessibilitySettings", controller: "AXMotionController", title: "MOTION_TITLE", table: titleTable)
                 }
                 // Spoken Content
-                SLink("SPEECH_TITLE".localize(table: titleTable), icon: "com.apple.graphic-icon.spoken-content") {
+                SLink("SPEECH_TITLE".localized(path: settings, table: titleTable), icon: "com.apple.graphic-icon.spoken-content") {
                     BundleControllerView("AccessibilitySettings", controller: "SpeechController", title: "SPEECH_TITLE", table: titleTable)
                 }
                 if !UIDevice.IsSimulator {
                     // Audio Descriptions
-                    SLink("DESCRIPTIVE_VIDEO_SETTING".localize(table: titleTable), icon: "com.apple.AccessibilityUIServer.audio.descriptions", status: "OFF".localize(table: table)) {}
+                    SLink("DESCRIPTIVE_VIDEO_SETTING".localized(path: settings, table: titleTable), icon: "com.apple.AccessibilityUIServer.audio.descriptions", status: "OFF".localized(path: settings, table: table)) {}
                 }
             } header: {
-                Text("VISION", tableName: titleTable)
+                Text("VISION".localized(path: settings, table: titleTable))
             }
             
             // MARK: Physical and Motor
             Section {
                 if !UIDevice.IsSimulator {
                     // Touch
-                    SLink("TOUCH".localize(table: titleTable), icon: "com.apple.AccessibilityUIServer.touch") {
+                    SLink("TOUCH".localized(path: settings, table: titleTable), icon: "com.apple.AccessibilityUIServer.touch") {
                         BundleControllerView("AccessibilitySettings", controller: "AXTouchAndReachability", title: "TOUCH", table: titleTable)
                     }
                 }
                 if UIDevice.PearlIDCapability {
                     // Face ID & Attention
-                    SLink("FACE_ID".localize(table: titleTable), icon: "com.apple.graphic-icon.face-id") {
+                    SLink("FACE_ID".localized(path: settings, table: titleTable), icon: "com.apple.graphic-icon.face-id") {
                         FaceAttentionView()
                     }
                 }
                 if !UIDevice.IsSimulator {
                     // Switch Control
-                    SLink("ScannerSwitchTitle".localize(table: titleTable), icon: "com.apple.AccessibilityUIServer.switch.control", status: "OFF".localize(table: table)) {}
+                    SLink("ScannerSwitchTitle".localized(path: settings, table: titleTable), icon: "com.apple.AccessibilityUIServer.switch.control", status: "OFF".localized(path: settings, table: table)) {}
                     // Voice Control
-                    SLink("CommandAndControlTitle".localize(table: titleTable), icon: "com.apple.AccessibilityUIServer.voice.control", status: "OFF".localize(table: table)) {}
+                    SLink("CommandAndControlTitle".localized(path: settings, table: titleTable), icon: "com.apple.AccessibilityUIServer.voice.control", status: "OFF".localized(path: settings, table: table)) {}
                     // Eye Tracking
-                    SLink("Eye Tracking", icon: "com.apple.AccessibilityUIServer.eye.tracking", status: "OFF".localize(table: table)) {}
+                    SLink("Eye Tracking", icon: "com.apple.AccessibilityUIServer.eye.tracking", status: "OFF".localized(path: settings, table: table)) {}
                     if UIDevice.iPhone {
                         // Side Button
-                        SLink("SIDE_CLICK_TITLE".localize(table: titleTable), icon: "com.apple.AccessibilityUIServer.side.button") {}
+                        SLink("SIDE_CLICK_TITLE".localized(path: settings, table: titleTable), icon: "com.apple.AccessibilityUIServer.side.button") {}
                         if UIDevice.AdvancedPhotographicStylesCapability {
                             // Camera Button
-                            SLink("CAMERA_BUTTON_TITLE".localize(table: cameraTable), icon: "com.apple.AccessibilityUIServer.camera.button") {}
+                            SLink("CAMERA_BUTTON_TITLE".localized(path: path, table: cameraTable), icon: "com.apple.AccessibilityUIServer.camera.button") {}
                         }
                         // Apple Watch Mirroring
-                        SLink("APPLE_WATCH_REMOTE_SCREEN".localize(table: table), icon: "com.apple.AccessibilityUIServer.applewatch.remote.mirroring") {}
+                        SLink("APPLE_WATCH_REMOTE_SCREEN".localized(path: settings, table: table), icon: "com.apple.AccessibilityUIServer.applewatch.remote.mirroring") {}
                     } else {
                         // Top Button
-                        SLink("TOP_CLICK_TITLE".localize(table: table), icon: "com.apple.AccessibilityUIServer.top.button") {}
+                        SLink("TOP_CLICK_TITLE".localized(path: settings, table: titleTable), icon: "com.apple.AccessibilityUIServer.top.button") {}
                         // Apple Pencil
-                        SLink("PencilTitle".localize(table: table), icon: "com.apple.AccessibilityUIServer.pencil") {}
+                        SLink("PencilTitle".localized(path: settings, table: titleTable), icon: "com.apple.AccessibilityUIServer.pencil") {}
                     }
                 }
                 if UIDevice.iPhone {
                     // Control Nearby Devices
-                    SLink("CONTROL_NEARBY_DEVICES".localize(table: table), icon: "com.apple.AccessibilityUIServer.control.nearby.devices") {
+                    SLink("CONTROL_NEARBY_DEVICES".localized(path: settings, table: table), icon: "com.apple.AccessibilityUIServer.control.nearby.devices") {
                         ControlNearbyDevicesView()
                     }
                 }
             } header: {
-                Text("MOBILITY_HEADING", tableName: titleTable)
+                Text("MOBILITY_HEADING".localized(path: settings, table: table))
             }
             
             // MARK: Hearing
             Section {
                 if !UIDevice.IsSimulator {
                     // Hearing Devices
-                    SLink("HEARING_AID_TITLE".localize(table: titleTable), icon: "com.apple.AccessibilityUIServer.hearing.devices") {}
+                    SLink("HEARING_AID_TITLE".localized(path: settings, table: table), icon: "com.apple.AccessibilityUIServer.hearing.devices") {}
                     // Hearing Control Center
-                    SLink("HEARING_CONTROL_CENTER_TITLE".localize(table: table), icon: "com.apple.AccessibilityUIServer.hearing.control.center") {}
+                    SLink("HEARING_CONTROL_CENTER_TITLE".localized(path: settings, table: table), icon: "com.apple.AccessibilityUIServer.hearing.control.center") {}
                     // Sound Recognition
-                    SLink("SOUND_RECOGNITION_TITLE".localize(table: titleTable), icon: "com.apple.AccessibilityUIServer.sound.recognition", status: "OFF".localize(table: table)) {}
+                    SLink("SOUND_RECOGNITION_TITLE".localized(path: settings, table: table), icon: "com.apple.AccessibilityUIServer.sound.recognition", status: "OFF".localized(path: settings, table: table)) {}
                     // RTT/TTY
-                    SLink("TTY_RTT_LABEL".localize(table: titleTable), icon: "com.apple.AccessibilityUIServer.rtt.tty", status: "OFF".localize(table: table)) {}
+                    SLink("TTY_RTT_LABEL".localized(path: settings, table: table), icon: "com.apple.AccessibilityUIServer.rtt.tty", status: "OFF".localized(path: settings, table: table)) {}
                     // Audio & Visual
-                    SLink("AUDIO_VISUAL_TITLE".localize(table: titleTable), icon: "com.apple.AccessibilityUIServer.audio.visual") {}
+                    SLink("AUDIO_VISUAL_TITLE".localized(path: settings, table: table), icon: "com.apple.AccessibilityUIServer.audio.visual") {}
                 }
                 // Subtitles & Captioning
-                SLink("SUBTITLES_CAPTIONING".localize(table: titleTable), icon: "com.apple.AccessibilityUIServer.subtitles.captioning") {
+                SLink("SUBTITLES_CAPTIONING".localized(path: settings, table: table), icon: "com.apple.AccessibilityUIServer.subtitles.captioning") {
                     BundleControllerView("AccessibilitySettings", controller: "AXCaptioningController", title: "SUBTITLES_CAPTIONING", table: titleTable)
                 }
                 if !UIDevice.IsSimulator {
                     // Live Captions
-                    SLink("RTT_LIVE_TRANSCRIPTIONS_LABEL".localize(table: table), icon: "com.apple.AccessibilityUIServer.live.captions") {}
+                    SLink("RTT_LIVE_TRANSCRIPTIONS_LABEL".localized(path: settings, table: table), icon: "com.apple.AccessibilityUIServer.live.captions") {}
                     if UIDevice.iPhone {
                         // Music Haptics
-                        SLink("HAPTIC_MUSIC_TITLE".localize(table: hapticTable), icon: "com.apple.AccessibilityUIServer.music.haptics", status: "OFF".localize(table: table)) {}
+                        SLink("HAPTIC_MUSIC_TITLE".localized(path: path, table: hapticTable), icon: "com.apple.AccessibilityUIServer.music.haptics", status: "OFF".localized(path: settings, table: table)) {}
                     }
                 }
             } header: {
-                Text("HEARING", tableName: titleTable)
+                Text("HEARING".localized(path: settings, table: table))
             }
             
             // MARK: Speech
             Section {
                 // Live Speech
-                SLink("LIVE_SPEECH_TITLE".localize(table: titleTable), icon: "com.apple.AccessibilityUIServer.live.speech", status: "OFF".localize(table: table)) {
+                SLink("LIVE_SPEECH_TITLE".localized(path: settings, table: table), icon: "com.apple.AccessibilityUIServer.live.speech", status: "OFF".localize(table: table)) {
                     BundleControllerView("AccessibilitySettings", controller: "LiveSpeechController", title: "LIVE_SPEECH_TITLE", table: titleTable)
                 }
-                if UIDevice.IsSimulator {
-                    // Vocal Shortcuts
-                    NavigationLink {
-                        Text(String())
-                            .onAppear {
-                                exit(0)
-                            }
-                    } label: {
-                        SettingsLink("ADAPTIVE_VOICE_SHORTCUTS_TITLE".localize(table: titleTable), status: "OFF".localize(table: table), destination: EmptyView())
-                    }
-                } else {
+                if !UIDevice.IsSimulator {
                     // Personal Voice
-                    SLink("PERSONAL_VOICE_TITLE".localize(table: titleTable), icon: "com.apple.AccessibilityUIServer.personal.voice") {}
+                    SLink("PERSONAL_VOICE_TITLE".localized(path: settings, table: table), icon: "com.apple.AccessibilityUIServer.personal.voice") {}
                     // Vocal Shortcuts
-                    SLink("ADAPTIVE_VOICE_SHORTCUTS_TITLE".localize(table: titleTable), icon: "com.apple.AccessibilityUIServer.vocal.shortcuts", status: "OFF".localize(table: table)) {}
+                    SLink("ADAPTIVE_VOICE_SHORTCUTS_TITLE".localized(path: settings, table: table), icon: "com.apple.AccessibilityUIServer.vocal.shortcuts", status: "OFF".localize(table: table)) {}
                 }
             } header: {
-                Text("SPEECH_HEADING", tableName: titleTable)
+                Text("SPEECH_HEADING".localized(path: settings, table: table))
             }
             
             // MARK: Accessories
             Section {
                 // Keyboards & Typing
-                SLink("KEYBOARDS".localize(table: titleTable), icon: "com.apple.graphic-icon.keyboard") {
+                SLink("KEYBOARDS".localized(path: settings, table: table), icon: "com.apple.graphic-icon.keyboard") {
                     BundleControllerView("AccessibilitySettings", controller: "AXKeyboardsController", title: "KEYBOARDS", table: titleTable)
                 }
                 if !UIDevice.IsSimulator {
                     // Apple TV Remote
-                    SLink("APPLE_TV_REMOTE".localize(table: titleTable), icon: "com.apple.graphic-icon.apple-tv-remote-settings") {}
+                    SLink("APPLE_TV_REMOTE".localized(path: settings, table: table), icon: "com.apple.graphic-icon.apple-tv-remote-settings") {}
                 }
             } header: {
-                Text("ACCESSORIES_HEADING", tableName: titleTable)
+                Text("ACCESSORIES_HEADING".localized(path: settings, table: table))
             }
             
             // MARK: General
             Section {
                 if !UIDevice.IsSimulator {
                     // Guided Access
-                    SLink("GUIDED_ACCESS_TITLE".localize(table: titleTable), icon: "com.apple.AccessibilityUIServer.guided.access", status: "OFF".localize(table: table)) {}
+                    SLink("GUIDED_ACCESS_TITLE".localized(path: settings, table: table), icon: "com.apple.AccessibilityUIServer.guided.access", status: "OFF".localized(path: settings, table: table)) {}
                     // Assistive Access
-                    SLink("CLARITY_UI_TITLE".localize(table: titleTable), icon: "com.apple.AccessibilityUIServer.assistive.access") {}
+                    SLink("CLARITY_UI_TITLE".localized(path: settings, table: titleTable), icon: "com.apple.AccessibilityUIServer.assistive.access") {}
                     // Siri
                     if siriEnabled {
-                        SLink("SIRI_SETTINGS_TITLE".localize(table: titleTable), icon: UIDevice.IntelligenceCapability ? "com.apple.application-icon.siri-intelligence" : "com.apple.application-icon.siri") {}
+                        SLink("SIRI_SETTINGS_TITLE".localized(path: settings, table: table), icon: UIDevice.IntelligenceCapability ? "com.apple.application-icon.siri-intelligence" : "com.apple.application-icon.siri") {}
                     }
                     // Accessibility Shortcut
-                    SLink("TRIPLE_CLICK_TITLE".localize(table: titleTable), icon: "com.apple.graphic-icon.accessibility", status: "OFF".localize(table: table)) {}
+                    SLink("TRIPLE_CLICK_TITLE".localized(path: settings, table: table), icon: "com.apple.graphic-icon.accessibility", status: "OFF".localized(path: settings, table: table)) {}
                     // Per-App Settings
-                    SLink("APP_AX_SETTINGS_TITLE".localize(table: titleTable), icon: "com.apple.AccessibilityUIServer.per.app.settings") {}
+                    SLink("APP_AX_SETTINGS_TITLE".localized(path: settings, table: titleTable), icon: "com.apple.AccessibilityUIServer.per.app.settings") {}
                 }
             } header: {
-                Text("GENERAL_HEADING", tableName: titleTable)
+                Text("GENERAL_HEADING".localized(path: settings, table: table))
             }
         }
         .onOpenURL { url in
