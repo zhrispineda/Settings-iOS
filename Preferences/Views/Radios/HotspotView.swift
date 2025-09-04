636dc2fb37a6e44c3d6f5b47158f2f7cbca7346c
zhrispineda <zhris@zhr.is>
2025-09-04 16:37:01 -0500

    Restore hotspot instructions

--- Preferences/Views/Radios/HotspotView.swift
+++ Preferences/Views/Radios/HotspotView.swift
@@ -40,6 +40,56 @@ struct HotspotView: View {
             } footer: {
                 Text("MAXIMIZE_COMPATIBILITY_FOOTER".localized(path: path, table: table))
             }
+            
+            // To connect using [Wi-Fi/Bluetooth/USB]
+            VStack(alignment: .leading, spacing: 20) {
+                HStack(alignment: .top) {
+                    Image(systemName: "wifi")
+                        .font(.title)
+                        .frame(width: 50)
+                    Text("""
+                                    \("CONNECT_OVER_WIFI_LABEL".localized(path: path, table: table, "WIFI"))
+                                    \("STEP_1".localized(path: path, table: table)) \("CONNECT_OVER_WIFI_STEP_1".localized(path: path, table: table, deviceName, "WIFI"))
+                                    \("STEP_2".localized(path: path, table: table)) \("CONNECT_OVER_WIFI_STEP_2".localized(path: path, table: table, "WIFI"))
+                                    """)
+                    .foregroundStyle(.secondary)
+                    .font(.footnote)
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity)
+                
+                HStack(alignment: .top) {
+                    Image(_internalSystemName: "bluetooth")
+                        .font(.title)
+                        .frame(width: 50)
+                    Text("""
+                        \("CONNECT_OVER_BLUETOOTH_LABEL".localized(path: path, table: table))
+                        \("STEP_1".localized(path: path, table: table)) \("\(UIDevice.iPhone ? "CONNECT_OVER_BLUETOOTH_STEP_1_IPHONE" : "CONNECT_OVER_BLUETOOTH_STEP_1_IPAD")".localized(path: path, table: table))
+                        \("STEP_2".localized(path: path, table: table)) \("\(UIDevice.iPhone ? "CONNECT_OVER_BLUETOOTH_STEP_2_IPHONE" : "CONNECT_OVER_BLUETOOTH_STEP_2_IPAD")".localized(path: path, table: table))
+                        \("STEP_3".localized(path: path, table: table)) \("\(UIDevice.iPhone ? "CONNECT_OVER_BLUETOOTH_STEP_3_IPHONE" : "CONNECT_OVER_BLUETOOTH_STEP_3_IPAD")".localized(path: path, table: table))
+                        """)
+                    .foregroundStyle(.secondary)
+                    .font(.footnote)
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity)
+                
+                HStack(alignment: .top) {
+                    Image(_internalSystemName: "usb")
+                        .font(.title)
+                        .frame(width: 50)
+                    Text("""
+                        \("CONNECT_OVER_USB_LABEL".localized(path: path, table: table))
+                        \("STEP_1".localized(path: path, table: table)) \("\(UIDevice.iPhone ? "CONNECT_OVER_USB_STEP_1_IPHONE" : "CONNECT_OVER_USB_STEP_1_IPAD")".localized(path: path, table: table))
+                        \("STEP_2".localized(path: path, table: table)) \("\(UIDevice.iPhone ? "CONNECT_OVER_USB_STEP_2_IPHONE" : "CONNECT_OVER_USB_STEP_2_IPAD")".localized(path: path, table: table))
+                        """)
+                    .foregroundStyle(.secondary)
+                    .font(.footnote)
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .listRowBackground(Color.clear)
         }
         .toolbar {
             ToolbarItem(placement: .principal) {
@@ -62,7 +112,7 @@ struct HotspotView: View {
     func randomPassword(length: Int = 20, separatorIndices: Set<Int> = [6, 13]) -> String {
         let letters = Array("abcdefghjkmnopqrstuvwxyz")
         var password = ""
-
+        
         for i in 0..<length {
             if separatorIndices.contains(i) {
                 password.append("-")
@@ -70,7 +120,7 @@ struct HotspotView: View {
                 password.append(randomChar)
             }
         }
-
+        
         return password
     }
 }
