f37a7af04d0dc13c3a6dfc6c2f11cf0b7a0e5b88
zhrispineda <zhris@zhr.is>
2025-09-19 11:34:36 -0500

    Update README for 17 build

--- Preferences/ContentView.swift
+++ Preferences/ContentView.swift
@@ -160,6 +160,24 @@ struct ContentView: View {
                             }
                         }
                     }
+                    
+//                    Section {
+//                        HStack {
+//                            if let asset = UIImage.asset(path: "/System/Library/PrivateFrameworks/MobileBackup.framework/PlugIns/MBPrebuddyFollowUpExtension.appex", name: "iPhone") {
+//                                Image(uiImage: asset)
+//                                    .resizable()
+//                                    .renderingMode(.template)
+//                                    .scaledToFit()
+//                                    .frame(width: 42)
+//                                    .foregroundStyle(.blue)
+//                            }
+//                            LabeledContent {} label: {
+//                                Text("Get Ready For Your New iPhone")
+//                                Text("Use iCloud to move your apps and data to your new iPhone.")
+//                            }
+//                        }
+//                        Button("Get Started") {}
+//                    }
 
                     if !UIDevice.IsSimulator {
                         // MARK: Radio Settings
