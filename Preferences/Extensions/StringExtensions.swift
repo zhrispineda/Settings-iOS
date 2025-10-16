f4bcbd28e48fefec956aa468fa608bfbbb7f5d5c
zhrispineda <zhris@zhr.is>
2025-10-16 14:51:40 -0400

    Update StringExtensions & UIImageExtensions to handle new 26.0.1 build edge case

--- Preferences/Extensions/StringExtensions.swift
+++ Preferences/Extensions/StringExtensions.swift
@@ -32,7 +32,7 @@ extension String {
             guard let newBuild = MGHelper.read(key: "mZfUC7qo4pURNhyMHZ62RQ") else {
                 return self
             }
-            newPath = "/Library/Developer/CoreSimulator/Volumes/iOS_\(newBuild)/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS \(UIDevice.current.systemVersion).simruntime/Contents/Resources/RuntimeRoot\(path)"
+            newPath = "/Library/Developer/CoreSimulator/Volumes/iOS_\(newBuild)/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS \(UIDevice.current.systemVersion == "26.0.1" ? "26.0" : UIDevice.current.systemVersion).simruntime/Contents/Resources/RuntimeRoot\(path)"
         } else {
             newPath = path
         }
--- Preferences/Extensions/UIImageExtensions.swift
+++ Preferences/Extensions/UIImageExtensions.swift
@@ -40,7 +40,7 @@ extension UIImage {
             guard let newBuild = MGHelper.read(key: "mZfUC7qo4pURNhyMHZ62RQ") else {
                 return nil
             }
-            newPath = "/Library/Developer/CoreSimulator/Volumes/iOS_\(newBuild)/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS \(UIDevice.current.systemVersion).simruntime/Contents/Resources/RuntimeRoot\(path)"
+            newPath = "/Library/Developer/CoreSimulator/Volumes/iOS_\(newBuild)/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS \(UIDevice.current.systemVersion == "26.0.1" ? "26.0" : UIDevice.current.systemVersion).simruntime/Contents/Resources/RuntimeRoot\(path)"
         } else {
             newPath = path
         }
