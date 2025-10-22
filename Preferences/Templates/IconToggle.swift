a3c0e80dc5d9cd7bc578efb927d9757f030cc405
zhrispineda <zhris@zhr.is>
2025-10-22 15:19:54 -0400

    Update IconToggle to clean up documentation and more unused logic

--- Preferences/Templates/IconToggle.swift
+++ Preferences/Templates/IconToggle.swift
@@ -1,8 +1,3 @@
-/*
-Abstract:
-A Toggle container with a rounded icon next to its label.
-*/
-
 import SwiftUI
 
 /// A Toggle container with a rounded icon next to its label.
@@ -10,33 +5,27 @@ import SwiftUI
 /// ```swift
 /// var body: some View {
 ///     List {
-///         IconToggle("Automatic Updates", color: Color.gray, icon: "gear")
+///         IconToggle("Software Updates", icon: "com.apple.graphic-icon.software-update")
 ///     }
 /// }
 /// ```
 ///
 /// - Parameter text: The String name of the label to display.
-/// - Parameter color: The Color to use as the icon background.
 /// - Parameter icon: The String name of the image asset or symbol.
 /// - Parameter subtitle: The String text of the subtitle to display.
-/// - Parameter iconColor: The Color to use for the icon.
 /// - Parameter table: The localization table to use for text.
 struct IconToggle: View {
     var text: String
     @Binding var isOn: Bool
-    var color: Color
     var icon: String
     var subtitle: String
-    var iconColor: Color
     var table: String
     
-    init(_ text: String, isOn: Binding<Bool>, color: Color = Color.clear, icon: String = "", subtitle: String = "", iconColor: Color = Color.white, table: String = "Localizable") {
+    init(_ text: String, isOn: Binding<Bool>, icon: String = "", subtitle: String = "", table: String = "Localizable") {
         self.text = text
         self._isOn = isOn
-        self.color = color
         self.icon = icon
         self.subtitle = subtitle
-        self.iconColor = iconColor
         self.table = table
     }
     
