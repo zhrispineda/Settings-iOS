efdaca3d71b2d4dbcc1ac94910d269af18997cec
zhrispineda <zhris@zhr.is>
2025-11-19 15:31:22 -0600

    Update DictionaryView for bridged view controller

--- Preferences/Views/Main/General/DictionaryView.swift
+++ Preferences/Views/Main/General/DictionaryView.swift
@@ -8,48 +8,14 @@
 import SwiftUI
 
 struct DictionaryView: View {
-    // Variables
-    struct Dictionary: Identifiable {
-        let id = UUID()
-        var language = String()
-        var dictionary = String()
-    }
-    
-    let dictionaries: [Dictionary] = [
-        Dictionary(language: "English (US)", dictionary: "New Oxford American Dictionary"),
-        Dictionary(language: "English (US)", dictionary: "Oxford American Writer‘s Thesaurus"),
-        Dictionary(language: "Apple Dictionary")
-    ]
-    
-    @State private var selected: [String] = []
+    let path = "/System/Library/PreferenceBundles/DictionarySettings.bundle"
     
     var body: some View {
-        CustomList(title: "Dictionary") {
-            ForEach(dictionaries) { dict in
-                Button {
-                    if let index = selected.firstIndex(of: dict.dictionary) {
-                        selected.remove(at: index)
-                    } else {
-                        selected.append(dict.dictionary)
-                    }
-                } label: {
-                    Label {
-                        VStack(alignment: .leading) {
-                            Text(dict.language)
-                            if !dict.dictionary.isEmpty {
-                                Text(dict.dictionary)
-                                    .foregroundStyle(.secondary)
-                                    .font(.caption)
-                            }
-                        }
-                    } icon: {
-                        Image(systemName: "checkmark")
-                            .opacity(selected.contains(dict.dictionary) ? 1 : 0)
-                    }
-                }
-                .foregroundStyle(.primary)
-            }
-        }
+        BundleControllerView(
+            "\(path)/DictionarySettings",
+            controller: "DictionarySettingsController",
+            title: "Dictionary".localized(path: path, table: "DictionarySettings")
+        )
     }
 }
 
