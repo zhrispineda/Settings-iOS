41a5408d41481efedf0f898e64bc042d714535d5
zhrispineda <zhris@zhr.is>
2025-11-21 14:46:44 -0600

    Update ContactsView to use bridge

--- Preferences/Views/Apps/ContactsView.swift
+++ Preferences/Views/Apps/ContactsView.swift
@@ -7,67 +7,20 @@
 
 import SwiftUI
 
+/// A bridge for the settings controller from Contacts.
+/// - Warning: This view may freeze in Xcode Previews, especially after scrolling down because the app cannot prompt for contacts permission.
+///
+/// **Workaround:** Use the app in Simulator or a physical device.
 struct ContactsView: View {
-    @State private var opacity = Double()
-    @State private var frameY = Double()
-    @State private var showingHelpSheet = false
-    let path = "/System/Library/PreferenceBundles/ContactsSettings.bundle"
-    let table = "Contacts"
-    
     var body: some View {
-        CustomList(title: "Back") {
-            Section {
-                Placard(title: "CONTACTS".localized(path: path, table: table), icon: "com.apple.MobileAddressBook", description: "SETTINGS_SUBTITLE".localized(path: path, table: table) + " [\("LEARN_MORE".localized(path: path, table: table))](pref://helpkit)", frameY: $frameY, opacity: $opacity)
-                Button("ADD_ACCOUNT".localized(path: path, table: table)) {}
-            }
-            
-            PermissionsView(appName: "CONTACTS".localized(path: path, table: table), cellular: false, location: false, notifications: false, cellularEnabled: .constant(false))
-            
-            Section {
-                Button {} label: {
-                    SettingsLink("Share Name and Photo".localized(path: path, table: table), status: "NAME_AND_PHOTO_SHARING_OFF".localized(path: path, table: table), destination: EmptyView())
-                }
-                .foregroundStyle(.primary)
-            } footer: {
-                Text("NAME_AND_PHOTO_SHARING_NOT_SHARING_FOOTER".localized(path: path, table: table))
-            }
-            
-            Section {
-                NavigationLink("Providers".localized(path: path, table: table)) {}
-            }
-            
-            Section {
-                SettingsLink("Sort Order".localized(path: path, table: table), status: "LAST".localized(path: path, table: table), destination: SelectOptionList("Sort Order", options: ["FIRST", "LAST"], selected: "LAST", table: table))
-                SettingsLink("Display Order".localized(path: path, table: table), status: "LAST".localized(path: path, table: table), destination: EmptyView())
-                NavigationLink("Short Name".localized(path: path, table: table)) {}
-                Button {} label: {
-                    NavigationLink("My Info".localized(path: path, table: table)) {}
-                }
-                .foregroundStyle(.primary)
-            }
-            
-            Section {
-                Button("Import SIM Contacts".localized(path: path, table: table)) {}
-            }
-        }
-        .toolbar {
-            ToolbarItem(placement: .principal) {
-                Text("CONTACTS".localized(path: path, table: table))
-                    .fontWeight(.semibold)
-                    .font(.subheadline)
-                    .opacity(frameY < 50.0 ? opacity : 0)
-            }
-        }
-        .onOpenURL { url in
-            if url.absoluteString == "pref://helpkit" {
-                showingHelpSheet = true
-            }
-        }
-        .sheet(isPresented: $showingHelpSheet) {
-            HelpKitView(topicID: UIDevice.iPhone ? "iph7edacccf9" : "ipad99df07d1")
-                .ignoresSafeArea(edges: .bottom)
-                .interactiveDismissDisabled()
-        }
+        BundleControllerView(
+            "ContactsSettings",
+            controller: "ContactsSettingsPlugin",
+            title: "CONTACTS".localized(
+                path: "/System/Library/PreferenceBundles/ContactsSettings.bundle",
+                table: "Contacts"
+            )
+        )
     }
 }
 
