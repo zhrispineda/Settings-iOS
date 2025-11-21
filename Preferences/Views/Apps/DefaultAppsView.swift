00d8a14e1e8c496d70b7844604f2ce9a5a14ee76
zhrispineda <zhris@zhr.is>
2025-11-21 15:15:31 -0600

    Update DefaultAppsView

--- Preferences/Views/Apps/DefaultAppsView.swift
+++ Preferences/Views/Apps/DefaultAppsView.swift
@@ -14,18 +14,35 @@ struct DefaultAppsView: View {
     
     var body: some View {
         CustomList(title: "Default Apps".localized(path: path)) {
-            Placard(title: "Default Apps".localized(path: path), icon: "com.apple.graphic-icon.default-apps", description: "Placard Subtitle".localized(path: path), frameY: $frameY, opacity: $opacity)
+            Placard(
+                title: "Default Apps".localized(path: path),
+                icon: "com.apple.graphic-icon.default-apps",
+                description: "Placard Subtitle".localized(path: path),
+                frameY: $frameY,
+                opacity: $opacity
+            )
             
             Section {
-                SettingsLink("Email".localized(path: path), status: "None".localized(path: path), destination: EmptyView())
-                SettingsLink("Messaging".localized(path: path), status: "Messages", destination: EmptyView())
-                SettingsLink("Calling".localized(path: path), status: "None".localized(path: path), destination: EmptyView())
+                SettingsLink("Email".localized(path: path), status: "None".localized(path: path)) {}
+                if !UIDevice.IsSimulator {
+                    SettingsLink("Messaging".localized(path: path), status: "Messages") {}
+                }
+                SettingsLink("Calling".localized(path: path), status: "None".localized(path: path)) {}
+                if !UIDevice.IsSimulator {
+                    SettingsLink("Call Filtering".localized(path: path), status: "None".localized(path: path)) {}
+                }
             }
             
             Section {
-                SettingsLink("Navigation", status: "Maps", destination: EmptyView())
-                SettingsLink("Browser App".localized(path: path), status: "Safari", destination: EmptyView())
-                SettingsLink("Translation", status: "None".localized(path: path), destination: EmptyView())
+                if UIDevice.IsSimulator {
+                    SettingsLink("Navigation", status: "Maps") {}
+                }
+                SettingsLink("Browser App".localized(path: path), status: "Safari") {}
+                SettingsLink("Translation", status: "None".localized(path: path)) {}
+                if !UIDevice.IsSimulator {
+                    SettingsLink("Passwords & Codes", status: "Passwords".localized(path: path)) {}
+                    SettingsLink("Contactless App", status: "Wallet".localized(path: path)) {}
+                }
             }
         }
         .toolbar {
