9cadb8f769d0dd73aa94aa8d2bedd4f83de83d70
zhrispineda <zhris@zhr.is>
2025-11-21 10:56:34 -0600

    Update view

--- Preferences/Views/Developer/InternalSettingsView.swift
+++ Preferences/Views/Developer/InternalSettingsView.swift
@@ -7,12 +7,17 @@ import SwiftUI
 
 struct InternalSettingsView: View {
     var body: some View {
-        CustomList(title: "Internal Settings") {
-            
+        CustomList(title: "Internal Settings", topPadding: true) {
+            Section("Device Info") {
+                NavigationLink("About") {}
+                Button("Set Up VPN") {}
+            }
         }
     }
 }
 
 #Preview {
-    InternalSettingsView()
+    NavigationStack {
+        InternalSettingsView()
+    }
 }
