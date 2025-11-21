e8448c750f3b1de06eecc9c99dae344d3d69832f
zhrispineda <zhris@zhr.is>
2025-11-21 11:17:09 -0600

    Update TranslateView to use bridge

--- Preferences/Views/Apps/TranslateView.swift
+++ Preferences/Views/Apps/TranslateView.swift
@@ -8,39 +8,14 @@
 import SwiftUI
 
 struct TranslateView: View {
-    @State private var onDeviceModeEnabled = false
-    @State private var showingSheet = false
-    let path = "/System/Library/PrivateFrameworks/TranslationUI.framework"
+    private let path = "/System/Library/PrivateFrameworks/TranslationUI.framework"
     
     var body: some View {
-        CustomList(title: "TRANSLATE".localized(path: path), topPadding: true) {
-            PermissionsView(appName: "TRANSLATE".localized(path: path), background: true, cellular: true, location: false, notifications: false, cellularEnabled: .constant(true))
-            
-            Section {
-                NavigationLink("ON_DEVICE_LANGUAGES_TITLE".localized(path: path), destination: DownloadedLanguagesView())
-            } footer: {
-                Text("ON_DEVICE_LANGUAGES_DESCRIPTION".localized(path: path))
-            }
-            
-            Section {
-                Toggle("ON_DEVICE_PREF_NAME".localized(path: path), isOn: $onDeviceModeEnabled)
-            } footer: {
-                Text("ON_DEVICE_FOOTER".localized(path: path))
-            }
-            
-            Section {} footer: {
-                Text("[\("PRIVACY_LINK".localized(path: path))](pref://translate)")
-            }
-        }
-        .onOpenURL { url in
-            if url.absoluteString == "pref://translate" {
-                showingSheet = true
-            }
-        }
-        .sheet(isPresented: $showingSheet) {
-            OnBoardingKitView(bundleID: "com.apple.onboarding.translate")
-                .ignoresSafeArea()
-        }
+        BundleControllerView(
+            "\(path)/TranslationUI",
+            controller: "LTUITranslateSettingsController",
+            title: "TRANSLATE".localized(path: path)
+        )
     }
 }
 
