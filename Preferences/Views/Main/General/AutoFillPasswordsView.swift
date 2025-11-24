52e31b2100ed5e65726205ab7be09915be0bf8f0
zhrispineda <zhris@zhr.is>
2025-11-24 14:59:54 -0600

    Update AutoFillPasswordsView for localization

--- Preferences/Views/Main/General/AutoFillPasswordsView.swift
+++ Preferences/Views/Main/General/AutoFillPasswordsView.swift
@@ -8,43 +8,47 @@
 import SwiftUI
 
 struct AutoFillPasswordsView: View {
-    // Variables
     @State private var autoFillPasswordPasskeysEnabled = true
     @State private var keychainEnabled = true
     @State private var deleteAfterUseEnabled = false
     @State private var selected = "Passwords"
     let options = ["Passwords"]
+    let path = "/System/Library/PrivateFrameworks/PasswordManagerUI.framework"
     
     var body: some View {
-        CustomList(title: "AutoFill & Passwords", topPadding: true) {
+        CustomList(title: "AutoFill & Passwords (navigation title)".localized(path: path), topPadding: true) {
             Section {
-                Toggle("AutoFill Passwords and Passkeys", isOn: $autoFillPasswordPasskeysEnabled)
+                Toggle("AutoFill Passwords and Passkeys".localized(path: path), isOn: $autoFillPasswordPasskeysEnabled)
             } footer: {
-                Text("Automatically suggest passwords, passkeys, and verification codes when signing in to apps and websites.")
+                Text("Automatically suggest passwords, passkeys, and verification codes when signing in to apps and websites.".localized(path: path))
             }
             
             Section {
-                IconToggle("Passwords", isOn: $keychainEnabled, icon: "com.apple.Passwords", subtitle: "Passkeys, passwords, and codes")
+                IconToggle(
+                    "Passwords".localized(path: path),
+                    isOn: $keychainEnabled,
+                    icon: "com.apple.Passwords",
+                    subtitle: "Passkeys, passwords, and codes".localized(path: path))
             } header: {
-                Text("AutoFill from:")
+                Text("AutoFill from:".localized(path: path))
             }
             
             Section {
-                Toggle("Delete After Use", isOn: $deleteAfterUseEnabled)
+                Toggle("Delete After Use".localized(path: path), isOn: $deleteAfterUseEnabled)
             } header: {
-                Text("Verification Codes")
+                Text("Verification Codes".localized(path: path))
             } footer: {
-                Text("Automatically delete verification codes in Messages and Mail after they are used.")
+                Text("Automatically delete verification codes in Messages and Mail after they are used.".localized(path: path))
             }
             
             Section {
                 HStack {
-                    Text("Set Up Codes In")
+                    Text("Set Up Codes In".localized(path: path))
                     Spacer()
                     Picker("", selection: $selected) {
                         ForEach(options, id: \.self) { option in
                             HStack {
-                                Text(" " + option)
+                                Text(" " + option.localized(path: path))
                                 Image(uiImage: UIImage.icon(forBundleID: "com.apple.Passwords")!)
                             }
                         }
@@ -52,10 +56,10 @@ struct AutoFillPasswordsView: View {
                     .frame(maxHeight: 30)
                     .labelsHidden()
                     .pickerStyle(.menu)
-                    .tint(.primary)
+                    .tint(.secondary)
                 }
             } footer: {
-                Text("Open verification code setup links and QR codes with this app.")
+                Text("“%@” will be used to open links and QR codes for setting up verification codes.".localized(path: path, selected.localized(path: path)))
             }
         }
     }
