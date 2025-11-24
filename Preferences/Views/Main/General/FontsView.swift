8ce0883b4ac13f6538d8f4d8a80a29a4fdda29eb
zhrispineda <zhris@zhr.is>
2025-11-24 15:50:30 -0600

    Update FontsView to use bridge

--- Preferences/Views/Main/General/FontsView.swift
+++ Preferences/Views/Main/General/FontsView.swift
@@ -8,23 +8,12 @@
 import SwiftUI
 
 struct FontsView: View {
-    let path = "/System/Library/PreferenceBundles/FontSettings.bundle"
-    
     var body: some View {
-        CustomList(title: "Fonts".localized(path: path)) {
-            Section {
-                NavigationLink("System Fonts".localized(path: path), destination: SystemFontsView())
-                NavigationLink("My Fonts".localized(path: path), destination: MyFontsView())
-            } footer: {
-                Text("INSTALLED_FONTS_CLARIFICATION".localized(path: path))
-            }
-            
-            Section {
-                Text("More Fonts".localized(path: path))
-            } footer: {
-                Text("MORE_FONTS_CLARIFICATION".localized(path: path))
-            }
-        }
+        BundleControllerView(
+            "FontSettings",
+            controller: "TopNavigationController",
+            title: "Fonts".localized(path: "/System/Library/PreferenceBundles/FontSettings.bundle")
+        )
     }
 }
 
