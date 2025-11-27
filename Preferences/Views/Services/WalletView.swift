d0b9f53eae76433840a49f04dca1099f29cb7f66
zhrispineda <zhris@zhr.is>
2025-11-26 18:38:07 -0600

    Clean up WalletView

--- Preferences/Views/Services/WalletView.swift
+++ Preferences/Views/Services/WalletView.swift
@@ -11,26 +11,51 @@ struct WalletView: View {
     @AppStorage("AppleCashEnabled") private var appleCashEnabled = false
     @State private var cellularEnabled = true
     @State private var hideExpiredPassesEnabled = true
-    let path = "/System/Library/Frameworks/PassKit.framework/"
-    let payTable = "Payment_Localizable"
-    let peerTable = "PeerPayment_Localizable"
-    let walletTable = "WalletSettings_Localizable"
+    private let path = "/System/Library/Frameworks/PassKit.framework"
+    private let payTable = "Payment_Localizable"
+    private let peerTable = "PeerPayment_Localizable"
+    private let walletTable = "WalletSettings_Localizable"
+    private var appleCashFooter: String {
+        if UIDevice.iPhone {
+            return UIDevice.PearlIDCapability ? "SETTINGS_ABOUT_FOOTER_FACEID_IPHONE" : "SETTINGS_ABOUT_FOOTER_IPHONE"
+        }
+        return "SETTINGS_ABOUT_FOOTER_IPAD"
+    }
     
     var body: some View {
         CustomList(title: "WALLET_&_APPLE_PAY".localized(path: path, table: walletTable), topPadding: true) {
             if UIDevice.IsSimulator {
-                PermissionsView(appName: "WALLET_&_APPLE_PAY".localized(path: path, table: walletTable), cellular: false, liveActivityToggle: true, location: false, siri: true, cellularEnabled: $cellularEnabled)
+                PermissionsView(
+                    appName: "WALLET_&_APPLE_PAY".localized(path: path, table: walletTable),
+                    cellular: false,
+                    liveActivityToggle: true,
+                    location: false,
+                    siri: true,
+                    cellularEnabled: $cellularEnabled
+                )
             } else {
-                PermissionsView(appName: "WALLET_&_APPLE_PAY".localized(path: path, table: walletTable), cellular: true, liveActivityToggle: true, location: false, siri: true, cellularEnabled: $cellularEnabled)
+                PermissionsView(
+                    appName: "WALLET_&_APPLE_PAY".localized(path: path, table: walletTable),
+                    cellular: true,
+                    liveActivityToggle: true,
+                    location: false,
+                    siri: true,
+                    cellularEnabled: $cellularEnabled
+                )
             }
             
             if !UIDevice.IsSimulator {
                 Section {
-                    SLink("SETTINGS_APPLE_PAY_DEFAULTS_TITLE".localized(path: path, table: payTable), icon: "com.apple.graphic-icon.account.payment", subtitle: "SETTINGS_APPLE_PAY_DEFAULTS_SUBTITLE".localized(path: path, table: payTable)) {}
+                    SLink(
+                        "SETTINGS_APPLE_PAY_DEFAULTS_TITLE".localized(path: path, table: payTable),
+                        icon: "com.apple.graphic-icon.account.payment",
+                        subtitle: "SETTINGS_APPLE_PAY_DEFAULTS_SUBTITLE".localized(path: path, table: payTable)
+                    ) {}
                 }
                 
                 Section {
-                    Toggle("SE_STORAGE_APPLET_CATEGORY_APLET_TYPE_PTC".localized(path: path, table: payTable), isOn: $appleCashEnabled.animation())
+                    Toggle("SE_STORAGE_APPLET_CATEGORY_APLET_TYPE_PTC".localized(path: path, table: payTable),
+                        isOn: $appleCashEnabled.animation())
                 } footer: {
                     if UIDevice.iPhone {
                         Text("PEER_PAYMENT_REGISTRATION_FOOTER_TEXT_IPHONE".localized(path: path, table: peerTable))
@@ -45,12 +70,19 @@ struct WalletView: View {
                     Text("SETTINGS_PAYMENT_CARDS_GROUP".localized(path: path, table: payTable))
                 } footer: {
                     if !appleCashEnabled {
-                        Text(.init("\(UIDevice.PearlIDCapability ? UIDevice.iPhone ? "SETTINGS_ABOUT_FOOTER_FACEID_IPHONE".localized(path: path, table: payTable) : "SETTINGS_ABOUT_FOOTER_FACEID_IPAD".localized(path: path, table: payTable) : UIDevice.iPad ? "SETTINGS_ABOUT_FOOTER_IPAD".localized(path: path, table: payTable) : "SETTINGS_ABOUT_FOOTER_IPHONE".localized(path: path, table: payTable)) [\("APPLE_PAY_PRIVACY".localized(path: path, table: walletTable))](#)"))
+                        Text(.init(
+                                appleCashFooter.localized(path: path, table: payTable) +
+                                " [\("APPLE_PAY_PRIVACY".localized(path: path, table: walletTable))](#)")
+                        )
                     }
                 }
             }
             
-            SLink("FPAN_ADD_TO_APPLE_PAY_TITLE".localized(path: path, table: payTable), icon: "None", status: "0") {}
+            SLink(
+                "FPAN_ADD_TO_APPLE_PAY_TITLE".localized(path: path, table: payTable),
+                icon: "None",
+                status: "0"
+            ) {}
             
             Section {
                 Toggle("ALLOW_EXPIRED_PASSES_TITLE".localized(path: path), isOn: $hideExpiredPassesEnabled)
