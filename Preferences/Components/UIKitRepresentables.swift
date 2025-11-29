4dbac6151d3709cec07bf8d0e554bdc06ef94c85
zhrispineda <zhris@zhr.is>
2025-11-29 12:26:36 -0600

    Add experimental PSFooterHyperlinkView bridge

--- Preferences/Components/UIKitRepresentables.swift
+++ Preferences/Components/UIKitRepresentables.swift
@@ -188,9 +188,7 @@ struct OBCombinedSplashView: UIViewControllerRepresentable {
             super.init(nibName: nil, bundle: nil)
         }
         
-        required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
+        required init?(coder: NSCoder) { fatalError() }
         
         override func viewDidLoad() {
             super.viewDidLoad()
@@ -241,10 +239,9 @@ struct OnBoardingKitView: UIViewControllerRepresentable {
             splashController.setValue(showLinkToPrivacyGateway, forKey: "showLinkToPrivacyGateway")
             splashController.setValue(showsLinkToUnifiedAbout, forKey: "showsLinkToUnifiedAbout")
             
-            let config = UIImage.SymbolConfiguration(pointSize: 18)
-            let checkmarkImage = UIImage(systemName: "checkmark", withConfiguration: config)
-            let dismissButton = UIBarButtonItem(image: checkmarkImage, style: .prominent, target: splashController, action: #selector(UIViewController.dismissView))
-            splashController.navigationItem.rightBarButtonItem = dismissButton
+            let checkmarkImage = UIImage(systemName: "xmark")
+            let dismissButton = UIBarButtonItem(image: checkmarkImage, style: .plain, target: splashController, action: #selector(UIViewController.dismissView))
+            splashController.navigationItem.leftBarButtonItem = dismissButton
             
             let navController = UINavigationController(rootViewController: splashController)
             return navController
@@ -407,9 +404,7 @@ class DebugMenuGestureProxyView: UIView {
         setupGestureRecognizer()
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("initWithCoder not supported")
-    }
+    required init?(coder: NSCoder) { fatalError() }
     
     private func setupGestureRecognizer() {
         let gesture = UITapGestureRecognizer(target: self, action: #selector(handleTwoFingerTap(_:)))
@@ -439,4 +434,100 @@ struct DebugMenuGestureView: UIViewRepresentable {
     }
     
     func updateUIView(_ uiView: UIView, context: Context) {}
+}
+
+struct PSFooterHyperlinkView: UIViewRepresentable {
+    let footerText: String
+    let linkText: String
+    let linkURL: String = "pref://"
+    let hideLinkPreview: Bool = true
+    let onLinkTap: (() -> Void)?
+    
+    func makeUIView(context: Context) -> UIView {
+        guard dlopen("/System/Library/PrivateFrameworks/Preferences.framework/Preferences", RTLD_NOW) != nil else {
+            return UIView()
+        }
+        
+        guard let specifierClass = NSClassFromString("PSSpecifier"),
+              let specifier = specifierClass.alloc() as? NSObject else {
+            return UIView()
+        }
+        
+        specifier.perform(NSSelectorFromString("init"))
+        
+        let setPropertySelector = NSSelectorFromString("setProperty:forKey:")
+        specifier.perform(setPropertySelector, with: footerText, with: "footerText")
+        specifier.perform(setPropertySelector, with: URL(string: linkURL)!, with: "headerFooterHyperlinkButtonURL")
+        
+        if let range = footerText.range(of: linkText) {
+            let nsRange = NSRange(range, in: footerText)
+            specifier.perform(setPropertySelector, with: "{\(nsRange.location), \(nsRange.length)}", with: "footerHyperlinkRange")
+        }
+        
+        if hideLinkPreview, let callback = onLinkTap {
+            context.coordinator.callback = callback
+            specifier.perform(setPropertySelector, with: NSStringFromSelector(#selector(Coordinator.customAction)), with: "footerHyperlinkAction")
+            specifier.perform(setPropertySelector, with: NSValue(nonretainedObject: context.coordinator), with: "footerHyperlinkTarget")
+        }
+        
+        guard let footerViewClass = NSClassFromString("PSFooterHyperlinkView"),
+              let footerView = footerViewClass.alloc() as? UIView else {
+            return UIView()
+        }
+        
+        footerView.perform(NSSelectorFromString("initWithSpecifier:"), with: specifier)
+        
+        for subview in footerView.subviews {
+            subview.directionalLayoutMargins = .zero
+        }
+        
+        return FooterLayoutUIView(view: footerView)
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+    
+    final class Coordinator: NSObject {
+        var callback: (() -> Void)?
+        
+        @objc func customAction() {
+            callback?()
+        }
+    }
+}
+
+final class FooterLayoutUIView: UIView {
+    let view: UIView
+
+    init(view: UIView) {
+        self.view = view
+        super.init(frame: .zero)
+        
+        addSubview(view)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        layoutMargins = .zero
+        view.layoutMargins = .zero
+        
+        NSLayoutConstraint.activate([
+            view.leadingAnchor.constraint(equalTo: leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: trailingAnchor),
+            view.topAnchor.constraint(equalTo: topAnchor),
+            view.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+
+    required init?(coder: NSCoder) { fatalError() }
+
+    override var intrinsicContentSize: CGSize {
+        let availableWidth = bounds.width > 0 ? bounds.width : (window?.windowScene?.screen.bounds.width ?? 320)
+        
+        return view.systemLayoutSizeFitting(
+            CGSize(width: availableWidth, height: UIView.layoutFittingCompressedSize.height),
+            withHorizontalFittingPriority: .required,
+            verticalFittingPriority: .fittingSizeLevel
+        )
+    }
 }
