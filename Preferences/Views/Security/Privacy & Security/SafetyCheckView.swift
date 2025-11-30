05a4f92d76af423dc09638e10b071660f3d50e1e
zhrispineda <zhris@zhr.is>
2025-11-30 15:47:47 -0600

    Clean up SafetyCheckView

--- Preferences/Views/Security/Privacy & Security/SafetyCheckView.swift
+++ Preferences/Views/Security/Privacy & Security/SafetyCheckView.swift
@@ -8,21 +8,21 @@
 import SwiftUI
 
 struct SafetyCheckView: View {
-    @State private var showingAlert = false
     let path = "/System/Library/PreferenceBundles/DigitalSeparationSettings.bundle"
     
     var body: some View {
-        CustomViewController("/System/Library/PreferenceBundles/DigitalSeparationSettings.bundle/DigitalSeparationSettings", controller: "DSSafetyCheckWelcomeController")
-            .ignoresSafeArea()
-            .navigationTitle("SAFETY_CHECK".localized(path: path))
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button("QUICK_EXIT".localized(path: path)) {
-                        exit(0)
-                    }
+        BundleControllerView(
+            "DigitalSeparationSettings",
+            controller: "DSSafetyCheckWelcomeController",
+            title: "SAFETY_CHECK".localized(path: path)
+        )
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("QUICK_EXIT".localized(path: path)) {
+                    exit(0)
                 }
             }
+        }
     }
 }
 
