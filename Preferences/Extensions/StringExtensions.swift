371a56685dab5c032c4d8bb375ad979a234e507e
zhrispineda <zhris@zhr.is>
2025-12-01 13:27:08 -0600

    Clean up StringExtensions

--- Preferences/Extensions/StringExtensions.swift
+++ Preferences/Extensions/StringExtensions.swift
@@ -1,28 +1,19 @@
 import SwiftUI
 
 extension String {
-    /// Returns a localized String given a table name and optional multi-argument values.
+    /// Returns a localized String given a path, optional table name, and optional multi-argument values.
     ///
     /// ```swift
     /// var body: some View {
-    ///      Text("KEY_TO_LOCALIZE".localize(table: "TableName")) // Table only
-    ///      Text("KEY_TO_LOCALIZE".localize(table: "TableName", "VARIABLE")) // Table and a single variable
-    ///      Toggle("KEY_TO_LOCALIZE".localize(table: "TableName"), isOn: .constant(true)) // Table only
-    ///      Toggle("KEY_TO_LOCALIZE".localize(table: "TableName", "VARIABLE"), isOn: .constant(true)) // Table and a single variable
-    ///      Toggle("KEY_TO_LOCALIZE".localize(table: "TableName", "FIRST_VARIALBE", "SECOND_VARIABLE", "THIRD_VARIABLE"), isOn: .constant(true)) // Table and several variables
+    ///      Text("KEY_TO_LOCALIZE".localized(path: "/Path/To/Example.bundle", table: "TableName", "Example var"))
     /// }
     /// ```
     ///
-    /// - Parameter table: The String table name to use.
-    /// - Parameter variables: The optional multi-parameter Strings to display as a variable or as variables.
-    /// - Returns: The localized String using the provided table.
-    func localize(table: String, _ variables: String...) -> String {
-        let format = NSLocalizedString(self, tableName: table, comment: "")
-        let localizedVariables = variables.map { String(localized: String.LocalizationValue($0), table: table) }
-        
-        return String(format: format, locale: .current, arguments: localizedVariables)
-    }
-    
+    /// - Parameter path: The path of the bundle.
+    /// - Parameter table: The name of the table.
+    /// - Parameter variables: Optional multi-parameter values to display.
+    ///
+    /// - Returns: The localized String using the provided path, table, and variables.
     @MainActor
     func localized(path: String, table: String = "Localizable", _ variables: CVarArg...) -> String {
         var newPath = ""
