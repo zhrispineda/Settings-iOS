e2ba917b4fa0d9d601c8a155f68b4d6e90e134ef
zhrispineda <zhris@zhr.is>
2025-12-02 13:26:43 -0600

    Clean up PermissionsView

--- Preferences/Templates/PermissionsView.swift
+++ Preferences/Templates/PermissionsView.swift
@@ -45,76 +45,141 @@ struct PermissionsView: View {
     @State private var faceIDEnabled = true
     @State private var liveActivityEnabled = true
     @State private var showingSheet = false
-    let path = "/System/Library/PrivateFrameworks/Preferences.framework"
-    let table = "PSSystemPolicy"
-    let notifPath = "/System/Library/PreferenceBundles/NotificationsSettings.bundle"
-    let notifTable = "NotificationsSettings"
-    let ICBSettings = "/System/Library/PreferenceBundles/ICBSettingsBundle.bundle"
-    let privacy = "/System/Library/OnBoardingBundles/com.apple.onboarding.maps.bundle"
-    let music = "/System/Library/PreferenceBundles/MusicSettings.bundle"
+    private let path = "/System/Library/PrivateFrameworks/Preferences.framework"
+    private let table = "PSSystemPolicy"
+    private let notifPath = "/System/Library/PreferenceBundles/NotificationsSettings.bundle"
+    private let notifTable = "NotificationsSettings"
+    private let ICBSettings = "/System/Library/PreferenceBundles/ICBSettingsBundle.bundle"
+    private let privacy = "/System/Library/OnBoardingBundles/com.apple.onboarding.maps.bundle"
+    private let music = "/System/Library/PreferenceBundles/MusicSettings.bundle"
     
     var body: some View {
         Section {
             if focus {
-                IconToggle("FOCUS".localized(path: path, table: table), isOn: $focusEnabled, icon: "com.apple.graphic-icon.focus")
+                IconToggle(
+                    "FOCUS".localized(path: path, table: table),
+                    isOn: $focusEnabled,
+                    icon: "com.apple.graphic-icon.focus"
+                )
             }
             if location {
-                SLink("LOCATION_SERVICES".localized(path: path, table: table), icon: "com.apple.graphic-icon.location", status: "NEVER_AUTHORIZATION".localized(path: "/System/Library/PrivateFrameworks/Settings/PrivacySettingsUI.framework", table: "LocationServicesPrivacy")) {}
+                SLink(
+                    "LOCATION_SERVICES".localized(path: path, table: table),
+                    icon: "com.apple.graphic-icon.location",
+                    status: "NEVER_AUTHORIZATION".localized(
+                        path: "/System/Library/PrivateFrameworks/Settings/PrivacySettingsUI.framework",
+                        table: "LocationServicesPrivacy"
+                    )
+                ) {}
             }
             if photos {
-                SLink("PHOTOS".localized(path: path, table: table), icon: "com.apple.mobileslideshow", status: "PHOTOS_LIMITED_AUTHORIZATION".localized(path: path, table: table)) {}
+                SLink(
+                    "PHOTOS".localized(path: path, table: table),
+                    icon: "com.apple.mobileslideshow",
+                    status: "PHOTOS_LIMITED_AUTHORIZATION".localized(path: path, table: table)
+                ) {}
             }
             if camera {
-                IconToggle("CAMERA".localized(path: path, table: table), isOn: $cameraEnabled, icon: "com.apple.graphic-icon.camera")
+                IconToggle(
+                    "CAMERA".localized(path: path, table: table),
+                    isOn: $cameraEnabled,
+                    icon: "com.apple.graphic-icon.camera"
+                )
             }
             if faceID {
-                IconToggle("FACE_ID".localized(path: path, table: "Pearl"), isOn: $faceIDEnabled, icon: "com.apple.graphic-icon.face-id")
+                IconToggle(
+                    "FACE_ID".localized(path: path, table: "Pearl"),
+                    isOn: $faceIDEnabled,
+                    icon: "com.apple.graphic-icon.face-id"
+                )
             }
             if siri {
                 if UIDevice.IntelligenceCapability {
-                    SLink("Apple Intelligence & Siri", icon: "com.apple.graphic-icon.intelligence") {
-                        SiriDetailView(appName: appName)
-                    }
+                    SLink(
+                        "Apple Intelligence & Siri",
+                        icon: "com.apple.graphic-icon.intelligence",
+                        destination: SiriDetailView(appName: appName)
+                    )
                 } else {
-                    SLink("Siri", icon: "com.apple.application-icon.siri") {
-                        SiriDetailView(appName: appName)
-                    }
+                    SLink(
+                        "Siri",
+                        icon: "com.apple.application-icon.siri",
+                        destination: SiriDetailView(appName: appName)
+                    )
                 }
-                SLink("Search", icon: "com.apple.graphic-icon.search") {
-                    SearchDetailView(appName: appName, appTitle: false)
-                }
+                SLink(
+                    "Search",
+                    icon: "com.apple.graphic-icon.search",
+                    destination: SearchDetailView(appName: appName, appTitle: false)
+                )
             }
             if notifications {
-                SLink("TITLE".localized(path: notifPath, table: notifTable), icon: "com.apple.graphic-icon.notifications", subtitle: appName == "Maps" ? "\("BANNER_ALERTS".localized(path: notifPath, table: notifTable)), \("SOUNDS".localized(path: notifPath, table: notifTable))" : "\("BANNER_ALERTS".localized(path: notifPath, table: notifTable)), \("SOUNDS".localized(path: notifPath, table: notifTable)), \("BADGES".localized(path: notifPath, table: notifTable))") {}
+                SLink(
+                    "TITLE".localized(path: notifPath, table: notifTable),
+                    icon: "com.apple.graphic-icon.notifications",
+                    subtitle: appName == "Maps" 
+                        ? "\("BANNER_ALERTS".localized(path: notifPath, table: notifTable)), \("SOUNDS".localized(path: notifPath, table: notifTable))"
+                        : "\("BANNER_ALERTS".localized(path: notifPath, table: notifTable)), \("SOUNDS".localized(path: notifPath, table: notifTable)), \("BADGES".localized(path: notifPath, table: notifTable))"
+                ) {}
             }
             if liveActivity {
-                SLink("LIVE_ACTIVITIES".localized(path: path, table: table), icon: "com.apple.graphic-icon.live-activities") {}
+                SLink(
+                    "LIVE_ACTIVITIES".localized(path: path, table: table),
+                    icon: "com.apple.graphic-icon.live-activities"
+                ) {}
             }
             if liveActivityToggle {
-                IconToggle("LIVE_ACTIVITIES".localized(path: path, table: table), isOn: $liveActivityEnabled, icon: "com.apple.graphic-icon.live-activities")
+                IconToggle(
+                    "LIVE_ACTIVITIES".localized(path: path, table: table),
+                    isOn: $liveActivityEnabled,
+                    icon: "com.apple.graphic-icon.live-activities"
+                )
             }
             if background {
-                IconToggle("BACKGROUND_APP_REFRESH".localized(path: path, table: table), isOn: $backgroundAppRefreshEnabled, icon: "com.apple.graphic-icon.background-app-refresh".localized(path: path, table: table))
+                IconToggle(
+                    "BACKGROUND_APP_REFRESH".localized(path: path, table: table),
+                    isOn: $backgroundAppRefreshEnabled,
+                    icon: "com.apple.graphic-icon.background-app-refresh".localized(path: path, table: table)
+                )
             }
             if cellular && UIDevice.CellularTelephonyCapability {
-                IconToggle("CELLULAR_DATA".localized(path: path, table: table), isOn: $cellularEnabled, icon: "com.apple.graphic-icon.cellular-settings")
+                IconToggle(
+                    "CELLULAR_DATA".localized(path: path, table: table),
+                    isOn: $cellularEnabled,
+                    icon: "com.apple.graphic-icon.cellular-settings"
+                )
             }
             if phone {
-                SLink("INCOMING_CALL_STYLE_LIST_BANNER".localized(path: ICBSettings, table: "ICBSettingsBundle"), icon: "com.apple.graphic-icon.incoming-phone-calls", status: "Banner") {}
-                SLink("ANNOUNCE_CALLS_TITLE".localized(path: "/System/Library/PrivateFrameworks/AssistantSettingsSupport.framework", table: "AssistantSettings"), icon: "com.apple.graphic-icon.announce-phone-calls", status: "NEVER".localized(path: "/System/Library/PrivateFrameworks/MessagesSettingsUI.framework", table: "MessagesSettings")) {}
+                SLink(
+                    "INCOMING_CALL_STYLE_LIST_BANNER".localized(path: ICBSettings, table: "ICBSettingsBundle"),
+                    icon: "com.apple.graphic-icon.incoming-phone-calls",
+                    status: "Banner"
+                ) {}
+                SLink(
+                    "ANNOUNCE_CALLS_TITLE".localized(
+                        path: "/System/Library/PrivateFrameworks/AssistantSettingsSupport.framework",
+                        table: "AssistantSettings"
+                    ),
+                    icon: "com.apple.graphic-icon.announce-phone-calls",
+                    status: "NEVER".localized(
+                        path: "/System/Library/PrivateFrameworks/MessagesSettingsUI.framework",
+                        table: "MessagesSettings"
+                    )
+                ) {}
             }
         } header: {
             Text("ALLOW_ACCESS_FORMAT".localized(path: path, table: table, appName))
         } footer: {
             if appName == "Maps" {
-                Text("[\("BUTTON_TITLE".localized(path: privacy, table: "Maps"))](pref://maps)")
+                PSFooterHyperlinkView(
+                    footerText: "BUTTON_TITLE".localized(path: privacy, table: "Maps"),
+                    linkText: "BUTTON_TITLE".localized(path: privacy, table: "Maps"),
+                    onLinkTap: {
+                        showingSheet = true
+                    }
+                )
             } else if appName == "Music" && UIDevice.CellularTelephonyCapability {
                 Text("CELLULAR_FOOTER_DOWNLOADS_ONLY".localized(path: music, table: "MusicSettings"))
-            }
-        }
-        .onOpenURL { url in
-            if url.absoluteString == "pref://maps" {
-                showingSheet = true
             }
         }
         .sheet(isPresented: $showingSheet) {
