fb7b3567673dcdb54910be207a1fa23b54fca8a5
zhrispineda <zhris@zhr.is>
2025-12-08 17:03:20 -0600

    Update MGHelper

--- Preferences/Components/MGHelper.swift
+++ Preferences/Components/MGHelper.swift
@@ -3,19 +3,21 @@
 //  Preferences
 //
 
-import Foundation
-
-/// Helper functions for interacting with MobileGestalt, an internal system for device characteristics lookup.
+/// Contains helper functions for interacting with MobileGestalt, an internal system for device characteristics lookup.
+///
+/// - Warning: Do not use methods in this class for public applications. It is not publicly supported.
 class MGHelper {
-    /// Returns the value of the given key from MobileGestalt.
+    /// Returns the value of a given key from MobileGestalt.
     ///
-    /// - Parameter key: The key as a String
-    /// - Returns: The value of the key as either a String value or nil
+    /// - Parameter key: The key to query as a String.
+    /// - Returns: The value of the key as either a String value or nil.
     ///
-    /// - Warning: Do not use this method for public apps. Usage is not recommended as it is not publicly supported and may be unstable.
+    /// - Warning: Do not use this for public applications. It is not publicly supported.
+    ///
+    /// - Note: Some keys may be inaccessible due to missing entitlements.
     static func read(key: String) -> String? {
-        typealias MGKey = (@convention(c) (CFString) -> CFTypeRef?)
-        var mgKey: MGKey?
+        typealias MGCopyAnswer = (@convention(c) (CFString) -> CFTypeRef?)
+        var mgCopyAnswer: MGCopyAnswer?
         
         // Initialize
         guard let gestalt = dlopen("/usr/lib/libMobileGestalt.dylib", RTLD_LAZY) else {
@@ -23,49 +25,50 @@ class MGHelper {
             return nil
         }
         
-        // Encode key
+        // Prepare key for use with MGCopyAnswer
         guard let key = CFStringCreateWithCString(nil, key, CFStringBuiltInEncodings.ASCII.rawValue) else {
-            SettingsLogger.error("Unable to encode key: \(key)")
+            SettingsLogger.error("Could not encode key: \(key)")
             return nil
         }
         
-        mgKey = unsafeBitCast(dlsym(gestalt, "MGCopyAnswer"), to: MGKey.self)
+        mgCopyAnswer = unsafeBitCast(dlsym(gestalt, "MGCopyAnswer"), to: MGCopyAnswer.self)
         
-        // Get key value
-        guard let value = mgKey?(key) else {
-            SettingsLogger.error("Could not get value for key: \(key)")
+        // Get answer through MGCopyAnswer
+        guard let value = mgCopyAnswer?(key) else {
+            SettingsLogger.error("Could not get answer for key: \(key)")
             return nil
         }
         
         let typeID = CFGetTypeID(value)
         
-        // Return as a casted String
+        // Return answer as a casted String
         switch typeID {
         case CFBooleanGetTypeID():
-            SettingsLogger.log("Found value for \(key): \(value)")
-            return CFBooleanGetValue((value as! CFBoolean)) ? "true" : "false"
+            let bool = value as! CFBoolean
+            SettingsLogger.log("Found (Bool) value for \(key): \(value)")
+            return CFBooleanGetValue(bool) ? "true" : "false"
         case CFNumberGetTypeID():
             if let number = value as? NSNumber {
-                SettingsLogger.info("Found value for \(key): \(value)")
+                SettingsLogger.info("Found (Number) value for \(key): \(value)")
                 return number.stringValue
             }
         case CFStringGetTypeID():
-            SettingsLogger.log("Found value for \(key): \(value)")
+            SettingsLogger.log("Found (String) value for \(key): \(value)")
             return String(describing: value)
         case CFDictionaryGetTypeID():
             if let dict = value as? [String: Any] {
-                SettingsLogger.log("Found value for \(key): \(value)")
+                SettingsLogger.log("Found (Dictionary) value for \(key): \(value)")
                 return "\(dict)"
             }
         case CFDataGetTypeID():
             let data = value as! Data
-            SettingsLogger.log("Found value for \(key): \(value)")
+            SettingsLogger.log("Found (Data) value for \(key): \(value)")
             return data.map { String($0) }.joined(separator: " ")
         default:
-            SettingsLogger.error("Could not get typeID for key: \(key)")
-            return nil
+            SettingsLogger.error("Could not get TypeID for key: \(key)")
         }
         
+        SettingsLogger.error("Could not get answer for key: \(key)")
         return nil
     }
 }
