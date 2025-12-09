efb71ac77091991d835fb2b1e302d4092e159308
zhrispineda <zhris@zhr.is>
2025-12-08 18:18:22 -0600

    Update SettingsTests

--- SettingsTests/SettingsTests.swift
+++ SettingsTests/SettingsTests.swift
@@ -7,23 +7,34 @@ import Testing
 @testable import Preferences
 
 struct SettingsTests {
-    // MARK: Identifier key
+    // MARK: - MobileGestalt
+    
+    /// This tests MobileGestalt to check if the keys currently used are still available.
+    /// Keys that cannot be read will default to an empty string, causing the test to fail.
+    ///
+    /// - Parameter key: The String key to query.
+    ///
+    /// - Warning: Do not use this for public applications. It is not publicly supported.
+    ///
+    /// - Note: Some keys accessible in Xcode Previews/Simulator may not be available on physical devices.
     @Test(arguments: [
-        "h9jDsbgj7xIVeIQ8S3/X3Q"
+        "h9jDsbgj7xIVeIQ8S3/X3Q", // ProductType
+        "97JDvERpVwO+GHtthIh7hA", // RegulatoryModelNumber
+        "D0cJ8r7U5zve6uA6QbOiLA" // ModelNumber
     ])
     func testMGKey(key: String) async throws {
         let result = MGHelper.read(key: key) ?? ""
         #expect(result.isEmpty == false)
     }
     
-    // MARK: Check Accessibility Settings
+    // MARK: - Accessibility Settings
+    
     @MainActor
     @Test func testSettingsAccessibilitySettingsExistence() throws {
         let accessibility = PrimarySettingsListModel.shared.mainSettings.contains(where: { $0.title == "Accessibility" })
         #expect(accessibility)
     }
     
-    // MARK: Check Accessibility Link
     @MainActor
     @Test func testSettingsAccessibilityShortcutSettingsExistence() throws {
         let accessibility = PrimarySettingsListModel.shared.mainSettings.contains(where: { $0.title == "Accessibility" })
