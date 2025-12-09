e589bcc3a60caef5205111c04e21e1d149cdc130
zhrispineda <zhris@zhr.is>
2025-12-09 16:46:38 -0600

    Move SUSSoftwareUpdateReleaseNotesDetail

---
+++ Preferences/Components/UIKit/SUSSoftwareUpdateReleaseNotesDetail.swift
@@ -0,0 +1,53 @@
+//
+//  SUSSoftwareUpdateReleaseNotesDetail.swift
+//  Preferences
+//
+
+import SwiftUI
+
+/// A UIViewRepresentable instance for `SUSSoftwareUpdateReleaseNotesDetail` from the SoftwareUpdateSettings framework.
+///
+/// - Parameter readMeName: The name of the ReadMe file.
+struct SUSSoftwareUpdateReleaseNotesDetail: UIViewControllerRepresentable {
+    let readMeName: String
+    
+    func makeUIViewController(context: Context) -> UIViewController {
+        dlopen("/System/Library/PrivateFrameworks/SoftwareUpdateSettings.framework/SoftwareUpdateSettings", RTLD_NOW)
+        
+        guard let controllerClass = NSClassFromString("SUSSoftwareUpdateReleaseNotesDetail") as? UIViewController.Type else {
+            SettingsLogger.error("Could not load SUSSoftwareUpdateReleaseNotesDetail")
+            return UIViewController()
+        }
+        
+        let instance = controllerClass.init()
+        
+        if let readMe = loadReadMe(named: readMeName) {
+            instance.setValue(readMe, forKey: "releaseNotes")
+        }
+        
+        return instance
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+    
+    private func loadReadMe(named fileName: String) -> String? {
+        guard let filePath = Bundle.main.path(forResource: fileName, ofType: "html") else {
+            SettingsLogger.error("Could not find ReadMe: \(fileName)")
+            return nil
+        }
+        
+        do {
+            return try String(contentsOfFile: filePath, encoding: .utf8)
+        } catch {
+            SettingsLogger.error("Could not load ReadMe: \(error)")
+            return nil
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        SUSSoftwareUpdateReleaseNotesDetail(readMeName: "iOSReadMeSummary")
+            .navigationTitle("Release Notes")
+    }
+}
--- Preferences/Components/UIKitRepresentables.swift
+++ Preferences/Components/UIKitRepresentables.swift
@@ -253,45 +253,6 @@ struct OnBoardingKitView: UIViewControllerRepresentable {
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
 
-// MARK: - SoftwareUpdateSettings
-/// SUSSoftwareUpdateReleaseNotesDetail for displaying release notes.
-struct ReleaseNotesViewController: UIViewControllerRepresentable {
-    let readMeName: String
-    
-    func makeUIViewController(context: Context) -> UIViewController {
-        dlopen("/System/Library/PrivateFrameworks/SoftwareUpdateSettings.framework/SoftwareUpdateSettings", RTLD_NOW)
-        
-        guard let controllerClass = NSClassFromString("SUSSoftwareUpdateReleaseNotesDetail") as? UIViewController.Type else {
-            SettingsLogger.error("Could not load SUSSoftwareUpdateReleaseNotesDetail")
-            return UIViewController()
-        }
-        
-        let instance = controllerClass.init()
-        
-        if let readMe = loadReadMe(named: readMeName) {
-            instance.setValue(readMe, forKey: "releaseNotes")
-        }
-        
-        return instance
-    }
-    
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
-    
-    private func loadReadMe(named fileName: String) -> String? {
-        guard let filePath = Bundle.main.path(forResource: fileName, ofType: "html") else {
-            SettingsLogger.error("Could not find ReadMe: \(fileName)")
-            return nil
-        }
-        
-        do {
-            return try String(contentsOfFile: filePath, encoding: .utf8)
-        } catch {
-            SettingsLogger.error("Could not load ReadMe: \(error)")
-            return nil
-        }
-    }
-}
-
 // MARK: - Other
 
 /// A view controller bridge based on a given path and class name.
