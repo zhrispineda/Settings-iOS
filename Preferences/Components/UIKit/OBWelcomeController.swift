00ce725d53f8b98639b1b221a575e5e765900397
zhrispineda <zhris@zhr.is>
2025-12-10 13:18:51 -0600

    Add OBWelcomeController

--- Preferences/Components/UIKit/OBWelcomeController.swift
+++ Preferences/Components/UIKit/OBWelcomeController.swift
@@ -2,17 +2,40 @@
 //  OBWelcomeController.swift
 //  Preferences
 //
-//  Created by Chris on 12/10/25.
-//
 
 import SwiftUI
 
-struct OBWelcomeController: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct OBWelcomeView: UIViewControllerRepresentable {
+    let title: String
+    let detailText: String
+    var contentLayout: Int64 = 2
+    
+    func makeUIViewController(context: Context) -> UIViewController {
+        guard let OBWelcomeControllerClass = NSClassFromString("OBWelcomeController") as? NSObject.Type else {
+            return UIViewController()
+        }
+        
+        let welcomeController = OBWelcomeControllerClass.perform(NSSelectorFromString("alloc")).takeUnretainedValue() as! NSObject
+        
+        let selector = NSSelectorFromString("initWithTitle:detailText:icon:contentLayout:")
+        
+        typealias InitFunction = @convention(c) (NSObject, Selector, NSString, NSString, NSObject?, Int64) -> UIViewController
+        
+        guard let method = welcomeController.method(for: selector) else {
+            return UIViewController()
+        }
+        
+        let initFunction = unsafeBitCast(method, to: InitFunction.self)
+        
+        return initFunction(
+            welcomeController,
+            selector,
+            title as NSString,
+            detailText as NSString,
+            nil as NSObject?,
+            contentLayout
+        )
     }
-}
-
-#Preview {
-    OBWelcomeController()
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
