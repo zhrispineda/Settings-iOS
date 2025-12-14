59bef281a2f61abdc9eaaaa97781c11cbe11cc4b
zhrispineda <zhris@zhr.is>
2025-12-13 20:02:44 -0600

    Update entitlements

---
+++ Preferences/Views/Attention/Notifications/EnhancedSafetyAlertsView.swift
@@ -0,0 +1,18 @@
+//
+//  EnhancedSafetyAlertsView.swift
+//  Preferences
+//
+//  Created by Chris on 12/13/25.
+//
+
+import SwiftUI
+
+struct EnhancedSafetyAlertsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EnhancedSafetyAlertsView()
+}
