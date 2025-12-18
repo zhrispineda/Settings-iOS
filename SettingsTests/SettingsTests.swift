f96aa647e50e3e26ee0bacdc287b148731f774b4
zhrispineda <zhris@zhr.is>
2025-12-18 15:20:52 -0600

    Update SettingsTests

--- SettingsTests/SettingsTests.swift
+++ SettingsTests/SettingsTests.swift
@@ -18,9 +18,23 @@ struct SettingsTests {
     ///
     /// - Note: Some keys accessible in Xcode Previews/Simulator may not be available on physical devices.
     @Test(arguments: [
+        "EqrsVvjcYDdxHBiQmGhAWw", // apple-internal-install
+        "mZfUC7qo4pURNhyMHZ62RQ", // BuildVersion
+        "2pxKjejpRGpWvUE+3yp5mQ", // cameraRestriction
+        "j8/Omm6s1lsmTDFsXjsBfA", // DeviceSupportsAlwaysOnTime
+        "qeaj75wk3HF4DwQ8qbIi7g", // DeviceSupportsEnhancedMultitasking
+        "uyejyEdaxNWSRQQwHmXz1A", // DiskUsage
+        "Pop5T2XQdDA60MRyxQJdQ", // hall-effect-sensor
+        "fh6DnnDGDVZ5kZ9nYn/GrQ", // hdr-image-capture
+        "JwLB44/jEB8aFDpXQ16Tuw", // HomeButtonType
+        "ulMliLomP737aAOJ/w/evA", // IsSimulator
+        "Z/dqyWS6OZTRy10UcmUAhw", // marketing-name
+        "D0cJ8r7U5zve6uA6QbOiLA", // ModelNumber
+        "8olRm6C1xqr7AJGpLRnpSw", // PearlIDCapability
         "h9jDsbgj7xIVeIQ8S3/X3Q", // ProductType
         "97JDvERpVwO+GHtthIh7hA", // RegulatoryModelNumber
-        "D0cJ8r7U5zve6uA6QbOiLA" // ModelNumber
+        "XYlJKKkj2hztRP1NWWnhlw", // ResearchFuse
+        "VasUgeSzVyHdB27g2XpN0g" // SerialNumber
     ])
     func testMGKey(key: String) async throws {
         let result = MGHelper.read(key: key) ?? ""
