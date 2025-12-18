8ecd0c1250452b54dbdd9b1223de3f1ca2580d8a
zhrispineda <zhris@zhr.is>
2025-12-18 15:18:11 -0600

    Add InternalAboutView

---
+++ Preferences/Views/Developer/Internal Settings/InternalAboutView.swift
@@ -0,0 +1,20 @@
+//
+//  InternalAboutView.swift
+//  Preferences
+//
+
+import SwiftUI
+
+struct InternalAboutView: View {
+    var body: some View {
+        CustomList(title: "About") {
+            
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        InternalAboutView()
+    }
+}
