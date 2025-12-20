7c82c1f501b32850d5332b03a8ae7e98e1791607
zhrispineda <zhris@zhr.is>
2025-12-20 11:20:21 -0600

    Remove asset from UIImageExtensions

--- Preferences/Extensions/UIImageExtensions.swift
+++ Preferences/Extensions/UIImageExtensions.swift
@@ -31,26 +31,4 @@ extension UIImage {
         let image = UIImage.perform(selector, with: bundleID)
         return image?.takeUnretainedValue() as? UIImage
     }
-    
-    /// Returns an UIImage from a bundle if it exists.
-    ///
-    /// ```swift
-    /// var body: some View {
-    ///      if let asset = UIImage.asset(path: "Example.bundle", name: "exampleIcon") {
-    ///         Image(uiImage: asset)
-    ///      }
-    /// }
-    /// ```
-    ///
-    /// - Parameter path: The path of the bundle.
-    /// - Parameter name: The name of the asset.
-    @MainActor
-    static func asset(path: String, name: String) -> UIImage? {
-        if let bundle = Bundle(path: UIDevice.RuntimePath + "\(path)") {
-            let image = UIImage(named: name, in: bundle, compatibleWith: nil)
-            return image
-        }
-        
-        return nil
-    }
 }
